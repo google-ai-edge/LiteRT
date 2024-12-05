486d1743ce1488a6e8084f32e393c8cb2f06880a
Google AI Edge <ai-edge-bot@google.com>
2024-12-04 17:42:49 -0800

    Add model_runtime_info_pb2 to lite.py
    PiperOrigin-RevId: 702920950

--- tflite/python/lite.py
+++ tflite/python/lite.py
@@ -32,6 +32,9 @@ from tensorflow.compiler.mlir.quantization.stablehlo import quantization_config_
 from tensorflow.compiler.mlir.quantization.tensorflow.python import representative_dataset as rd
 from tensorflow.core.framework import graph_pb2 as _graph_pb2
 from tflite.experimental.microfrontend.python.ops import audio_microfrontend_op  # pylint: disable=unused-import
+# The following imports are needed to make the model_runtime_info_pb2
+# and profiling_info_pb2 protos available via the litert PIP package.
+from tflite.profiling.proto import model_runtime_info_pb2  # pylint: disable=unused-import
 from tflite.profiling.proto import profiling_info_pb2  # pylint: disable=unused-import
 from tflite.python import conversion_metadata_schema_py_generated as conversion_metadata_fb
 from tflite.python import lite_constants as constants
