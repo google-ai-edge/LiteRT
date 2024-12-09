c40ad8cfdd42ac9264c9abbf00dce60b6027e279
Google AI Edge <ai-edge-bot@google.com>
2024-12-08 20:35:01 -0800

    Automated Code Change
    PiperOrigin-RevId: 704129182

--- tflite/delegates/flex/util.cc
+++ tflite/delegates/flex/util.cc
@@ -14,7 +14,11 @@ limitations under the License.
 ==============================================================================*/
 #include "tflite/delegates/flex/util.h"
 
+#include <cstdint>
+#include <cstring>
+#include <limits>
 #include <string>
+#include <vector>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
