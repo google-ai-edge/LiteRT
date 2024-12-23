4806e17527c13a50815d5fe0b9587486aba64bac
Google AI Edge <ai-edge-bot@google.com>
2024-12-22 21:14:00 -0800

    Automated Code Change
    PiperOrigin-RevId: 708941107

--- tflite/toco/graph_transformations/quantization_util.cc
+++ tflite/toco/graph_transformations/quantization_util.cc
@@ -14,6 +14,9 @@ limitations under the License.
 ==============================================================================*/
 #include "tflite/toco/graph_transformations/quantization_util.h"
 
+#include <cmath>
+#include <cstddef>
+#include <limits>
 #include <memory>
 #include <string>
 
