d956ff0b63eabc175e341e9b21baf8f580db3980
Google AI Edge <ai-edge-bot@google.com>
2024-12-22 22:13:02 -0800

    Automated Code Change
    PiperOrigin-RevId: 708950899

--- tflite/tensorflow_profiler_logger_shim.cc
+++ tflite/tensorflow_profiler_logger_shim.cc
@@ -13,6 +13,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#include <cstddef>
+
 #include "tflite/core/macros.h"
 #include "tflite/tensorflow_profiler_logger.h"
 
