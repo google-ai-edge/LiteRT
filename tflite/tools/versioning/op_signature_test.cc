ce6afc9adc037d83dadb744f5bb006163c8bdf32
Google AI Edge <ai-edge-bot@google.com>
2025-01-29 03:17:43 -0800

    Automated Code Change
    PiperOrigin-RevId: 720902667

--- tflite/tools/versioning/op_signature_test.cc
+++ tflite/tools/versioning/op_signature_test.cc
@@ -14,6 +14,7 @@ limitations under the License.
 ==============================================================================*/
 #include "tflite/tools/versioning/op_signature.h"
 
+#include <cstdlib>
 #include <cstring>
 #include <memory>
 #include <string>
