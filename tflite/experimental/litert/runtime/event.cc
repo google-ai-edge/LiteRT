0874e719f0750d4d92ee9311d5713c14c766bf30
Google AI Edge <ai-edge-bot@google.com>
2025-02-27 18:36:56 -0800

    Internal change to fix tests
    PiperOrigin-RevId: 731944706

--- tflite/experimental/litert/runtime/event.cc
+++ tflite/experimental/litert/runtime/event.cc
@@ -15,16 +15,17 @@
 #include "tflite/experimental/litert/runtime/event.h"
 
 #include <fcntl.h>
-#include <poll.h>
 #include <unistd.h>
 
 #include <cerrno>
 #include <cstdint>
-#include <cstring>
 
 #include "tflite/experimental/litert/c/litert_common.h"
-#include "tflite/experimental/litert/c/litert_logging.h"
 #include "tflite/experimental/litert/cc/litert_expected.h"
+
+#if LITERT_HAS_SYNC_FENCE_SUPPORT
+#include <poll.h>
+#endif  // LITERT_HAS_SYNC_FENCE_SUPPORT
 
 using litert::Error;
 using litert::Expected;
