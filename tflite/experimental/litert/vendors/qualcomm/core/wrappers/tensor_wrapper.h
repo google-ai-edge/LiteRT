5264423859f81b3b912a9dd30a9487af092287a9
Alen Huang <quic_weilhuan@quicinc.com>
2025-03-04 09:54:37 -0800

    PR #88221: Qualcomm AI Engine Direct - Op Builders for 1P Models
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/88221
    
    # WHAT
    1. Conv2d
    2. DepthwiseConv2d
    3. AveragePool
    4. MaxPool
    5. DepthToSpace
    6. SpaceToDepth
    7. HardSwish
    8. LeakyRelu
    9. ResizeBilinear
    10. Litert options for these op builders, unit test
    11. update qnn_compiler_plugin_test with these op builders
    
    # TEST
    - qnn_compiler_plugin_test
    ```
    [----------] Global test environment tear-down
    [==========] 115 tests from 5 test suites ran. (3489 ms total)
    [  PASSED  ] 115 tests.
    ```
    - litert_options_test
    ```
    [----------] Global test environment tear-down
    [==========] 22 tests from 1 test suite ran. (0 ms total)
    [  PASSED  ] 22 tests
    ```
    Copybara import of the project:
    
    --
    dca2c3b034e094ec61a3b4bf2d732f01fcb36cea by weilhuan-quic <quic_weilhuan@quicinc.com>:
    
    Qualcomm AI Engine Direct - Op Builders for 1P Models
    1. Conv2d
    2. DepthwiseConv2d
    3. AveragePool
    4. MaxPool
    5. DepthToSpace
    6. SpaceToDepth
    7. HardSwish
    8. LeakyRelu
    9. ResizeBilinear
    10. Litert options for these op builders, unit test
    11. update qnn_compiler_plugin_test with these op builders
    
    Merging this change closes #88221
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/88221 from jiunkaiy:dev/weilhuan/more_op_builders dca2c3b034e094ec61a3b4bf2d732f01fcb36cea
    PiperOrigin-RevId: 733366378

--- tflite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
+++ tflite/experimental/litert/vendors/qualcomm/core/wrappers/tensor_wrapper.h
@@ -51,6 +51,8 @@ class TensorWrapper final {
     return quantize_params_;
   };
 
+  QuantizeParamsWrapperVariant& GetQuantParams() { return quantize_params_; };
+
   bool IsPerTensorQuantWithOffsetDiff(const TensorWrapper& rhs) const;
 
   bool IsQuant8() const {
@@ -93,6 +95,15 @@ class TensorWrapper final {
   const void* GetStaticTensorData() const {
     return qnn_tensor_.v2.clientBuf.data;
   };
+
+  void ConvertAxisScaleOffsetToScaleOffset() {
+    if (!std::holds_alternative<AxisScaleOffsetQuantizeParamsWrapper>(
+            quantize_params_)) {
+      return;
+    }
+
+    quantize_params_.emplace<ScaleOffsetQuantizeParamsWrapper>(0.0, 0);
+  }
 
   size_t GetTensorSize() const;
 
