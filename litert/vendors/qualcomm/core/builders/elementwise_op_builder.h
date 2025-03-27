54190db070be3a0898f7171751727f95344660e7
weilhuan-quic <quic_weilhuan@quicinc.com>
2025-03-27 18:11:25 +0800

    Qualcomm AI Engine Direct - More Op Builders 1. ElementwiseFloorDiv 2. ElementwiseNotEqual 3. ElementwiseLogistic 4. Pad PadV2 5. MaxPool2d 6. GatherNd 7. Cumsum 8. Simple op model, unit tests 9. Support converting static tensor in TensorPool 10. TensorPool unit test

--- litert/vendors/qualcomm/core/builders/elementwise_op_builder.h
+++ litert/vendors/qualcomm/core/builders/elementwise_op_builder.h
@@ -68,6 +68,14 @@ std::vector<OpWrapper> BuildElementwiseMaximumOp(
     TensorPool& tensor_pool, const std::vector<TensorWrapperRef>& inputs,
     const std::vector<TensorWrapperRef>& outputs);
 
+std::vector<OpWrapper> BuildElementwiseFloorDivOp(
+    TensorPool& tensor_pool, const std::vector<TensorWrapperRef>& inputs,
+    const std::vector<TensorWrapperRef>& outputs);
+
+std::vector<OpWrapper> BuildElementwiseNotEqualOp(
+    TensorPool& tensor_pool, const std::vector<TensorWrapperRef>& inputs,
+    const std::vector<TensorWrapperRef>& outputs);
+
 }  // namespace qnn
 
 #endif  // ODML_LITERT_LITERT_VENDORS_QUALCOMM_CORE_BUILDERS_ELEMENTWISE_OP_BUILDER_H_
