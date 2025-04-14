2d50d1bcaf710aeddce341b43d7e44f907f01c0f
Ritul Jasuja <ritul.jasuja@intel.com>
2025-04-14 22:35:16 +0530

    Initial Implementation for Graph Iterator for OV Compiler plugin
    Integrate Decoder Tensor and Operation classes with GraphIterator
    Iterate through partitions in compiler_plugin.
    
    Signed-off-by: Ritul Jasuja <ritul.jasuja@intel.com>

--- litert/vendors/intel_openvino/compiler/graph_iterator.cc
+++ litert/vendors/intel_openvino/compiler/graph_iterator.cc
@@ -2,12 +2,12 @@
 // SPDX-License-Identifier: Apache-2.0
 
 #include "graph_iterator.h"
-#include "openvino/frontend/tensorflow_lite/quantization_info.hpp"
 
-namespace tflite {
-namespace openvinodelegate {
+namespace litert {
+namespace openvino {
 size_t GraphIteratorDelegate::size() const {
-    // TODO: implement this
+    return iterator_indices_.input_index_ + iterator_indices_.output_index_ +
+           iterator_indices_.op_index_;
 }
 
 void GraphIteratorDelegate::reset() { node_index_ = 0; }
@@ -21,13 +21,79 @@ bool GraphIteratorDelegate::is_end() const {
         return false;
 }
 
-std::shared_ptr<ov::frontend::tensorflow_lite::DecoderBase> GraphIteratorDelegate::get_decoder()
-    const {
-    // TODO: implement this
-    // Iterate through all nodes and i/o tensors from the subgraph
-    // to create nodes and Tensor in OV frontend expectation
+void fill_tensor_meta(ov::frontend::tensorflow_lite::TensorMetaInfo& tensor_meta_info,
+                      std::vector<int64_t> shape_vec, ov::element::Type ov_element_type,
+                      std::string name) {
+    ov::PartialShape tensor_shape{shape_vec};
+    tensor_meta_info.m_partial_shape = tensor_shape;
+    tensor_meta_info.m_element_type = ov_element_type;
+    tensor_meta_info.m_tensor_name = name;
 }
 
-size_t GraphIteratorDelegate::get_subgraph_size() const { return 0; }
-}  // namespace openvinodelegate
-}  // namespace tflite
+std::shared_ptr<ov::frontend::tensorflow_lite::DecoderBase> GraphIteratorDelegate::get_decoder()
+    const {
+    ov::frontend::tensorflow_lite::TensorMetaInfo tensor_meta_info;
+    std::vector<int64_t> shape_vec;
+    if (node_index_ < iterator_indices_.input_index_) {
+        const auto& input_vec = subgraph_ptr_->Inputs();
+        const auto& input = input_vec[node_index_];
+        const ElementType type = input.ElementType();
+        ov::element::Type ov_element_type = MapLiteTypeToOV(type);
+        if (GetOVTensorShape(input, shape_vec) != kLiteRtStatusOk) {
+            LITERT_LOG(LITERT_INFO, "Unsupported tensor element shape");
+        }
+        int64_t input_index = node_index_;
+        int64_t output_index = -1;
+        fill_tensor_meta(tensor_meta_info, shape_vec, ov_element_type, std::string(input.Name()));
+
+        return std::make_shared<DecoderTensor>(tensor_meta_info, input_index, output_index);
+    } else if (node_index_ >= iterator_indices_.input_index_ &&
+               node_index_ < iterator_indices_.input_index_ + iterator_indices_.output_index_) {
+        const auto& output_vec = subgraph_ptr_->Outputs();
+        const auto& output = output_vec[node_index_ - iterator_indices_.input_index_];
+        int64_t input_index = -1;
+        int64_t output_index = node_index_;
+        const ElementType type = output.ElementType();
+        ov::element::Type ov_element_type = MapLiteTypeToOV(type);
+        if (GetOVTensorShape(output, shape_vec) != kLiteRtStatusOk) {
+            LITERT_LOG(LITERT_INFO, "Unsupported tensor element shape");
+        }
+        fill_tensor_meta(tensor_meta_info, shape_vec, ov_element_type, std::string(output.Name()));
+        return std::make_shared<DecoderTensor>(tensor_meta_info, input_index, output_index);
+    } else {
+        const auto& op_vec = subgraph_ptr_->Ops();
+        const auto& op =
+            op_vec[node_index_ - iterator_indices_.input_index_ - iterator_indices_.output_index_];
+        std::vector<ov::frontend::tensorflow_lite::TensorMetaInfo> input_meta_info;
+        std::vector<ov::frontend::tensorflow_lite::TensorMetaInfo> output_meta_info;
+        for (const auto& input : op.Inputs()) {
+            const ElementType type = input.ElementType();
+            ov::element::Type ov_element_type = MapLiteTypeToOV(type);
+            if (GetOVTensorShape(input, shape_vec) != kLiteRtStatusOk) {
+                LITERT_LOG(LITERT_INFO, "Unsupported tensor element shape for op creation");
+            }
+            fill_tensor_meta(tensor_meta_info, shape_vec, ov_element_type,
+                             std::string(input.Name()));
+            if (input.HasWeights()) {
+                LITERT_LOG(LITERT_INFO, "Data is static or constant");
+                tensor_meta_info.m_tensor_data = input.Weights().Bytes().data();
+            }
+            input_meta_info.push_back(tensor_meta_info);
+        }
+        for (const auto& output : op.Outputs()) {
+            const ElementType type = output.ElementType();
+            ov::element::Type ov_element_type = MapLiteTypeToOV(type);
+            if (GetOVTensorShape(output, shape_vec) != kLiteRtStatusOk) {
+                LITERT_LOG(LITERT_INFO, "Unsupported tensor element shape for op creation");
+            }
+            fill_tensor_meta(tensor_meta_info, shape_vec, ov_element_type,
+                             std::string(output.Name()));
+            output_meta_info.push_back(tensor_meta_info);
+        }
+        return std::make_shared<DecoderOperation>(input_meta_info, output_meta_info, op,
+                                                  node_index_);
+    }
+}
+
+}  // namespace openvino
+}  // namespace litert
