cbbb686512d2eec7d79c735c096dd172f530ddd6
chuntl <quic_chuntl@quicinc.com>
2025-04-15 16:29:38 +0800

    Qualcomm AI Engine Direct - Bypass int16 for RMSNorm
    Summary:
    - Let RMSNorm pass the validation
    - Disable int16ToUint16 feature
    - Leave TODO to remove later

--- litert/vendors/qualcomm/compiler/qnn_compiler_plugin.cc
+++ litert/vendors/qualcomm/compiler/qnn_compiler_plugin.cc
@@ -265,7 +265,7 @@ LiteRtStatus LiteRtCompilerPluginPartition(LiteRtCompilerPlugin compiler_plugin,
         op, tensor_pool, input_tensors, output_tensors, op_wrappers));
     tensor_pool.ForEach([](::qnn::TensorWrapper& tensor_wrapper) {
       // TODO(chunhsue): Use compile interface to get useQInt16AsQUint16.
-      constexpr bool useQInt16AsQUint16 = true;
+      constexpr bool useQInt16AsQUint16 = false;
       if constexpr (useQInt16AsQUint16) {
         tensor_wrapper.ConvertQint16ToQuint16();
       }
