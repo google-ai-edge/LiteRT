ecfabe7446be4e55782ebf0ec39cb09c714637e9
Quentin Khan <qkhan@google.com>
2025-04-24 07:21:36 -0700

    Add CPU accelerator options.
    LiteRT-PiperOrigin-RevId: 750985361

---
+++ litert/c/options/litert_cpu_options.cc
@@ -0,0 +1,107 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "litert/c/options/litert_cpu_options.h"
+
+#include <memory>
+
+#include "litert/c/litert_common.h"
+#include "litert/c/litert_opaque_options.h"
+#include "litert/cc/litert_macros.h"
+#include "tflite/delegates/xnnpack/xnnpack_delegate.h"
+
+struct LiteRtCpuOptionsT {
+  TfLiteXNNPackDelegateOptions xnn = TfLiteXNNPackDelegateOptionsDefault();
+};
+
+LiteRtStatus LiteRtCreateCpuOptions(LiteRtOpaqueOptions* options) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  auto options_data = std::make_unique<LiteRtCpuOptionsT>();
+  LITERT_RETURN_IF_ERROR(LiteRtCreateOpaqueOptions(
+      LiteRtGetCpuOptionsIdentifier(), options_data.get(),
+      [](void* payload) { delete reinterpret_cast<LiteRtCpuOptions>(payload); },
+      options));
+  options_data.release();
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtFindCpuOptions(LiteRtOpaqueOptions opaque_options,
+                                  LiteRtCpuOptions* cpu_options) {
+  LITERT_RETURN_IF_ERROR(cpu_options,
+                         litert::ErrorStatusBuilder::InvalidArgument())
+      << "cpu_options is null.";
+  void* options_data = nullptr;
+  LITERT_RETURN_IF_ERROR(LiteRtFindOpaqueOptionsData(
+      opaque_options, LiteRtGetCpuOptionsIdentifier(), &options_data));
+  *cpu_options = reinterpret_cast<LiteRtCpuOptions>(options_data);
+  return kLiteRtStatusOk;
+}
+
+const char* LiteRtGetCpuOptionsIdentifier() { return "xnnpack"; }
+
+LiteRtStatus LiteRtSetCpuOptionsNumThread(LiteRtCpuOptions options,
+                                          int num_threads) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  options->xnn.num_threads = num_threads;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtGetCpuOptionsNumThread(LiteRtCpuOptionsConst options,
+                                          int* const num_threads) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  LITERT_RETURN_IF_ERROR(num_threads,
+                         litert::ErrorStatusBuilder::InvalidArgument())
+      << "num_threads is null.";
+  *num_threads = options->xnn.num_threads;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtSetCpuOptionsXNNPackFlags(LiteRtCpuOptions options,
+                                             int flags) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  options->xnn.flags = flags;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtGetCpuOptionsXNNPackFlags(LiteRtCpuOptionsConst options,
+                                             int* const flags) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  LITERT_RETURN_IF_ERROR(flags, litert::ErrorStatusBuilder::InvalidArgument())
+      << "flags is null.";
+  *flags = options->xnn.flags;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtSetCpuOptionsXnnPackWeightCachePath(LiteRtCpuOptions options,
+                                                       const char* path) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  options->xnn.weight_cache_file_path = path;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtGetCpuOptionsXnnPackWeightCachePath(
+    LiteRtCpuOptionsConst options, const char** const path) {
+  LITERT_RETURN_IF_ERROR(options, litert::ErrorStatusBuilder::InvalidArgument())
+      << "options is null.";
+  LITERT_RETURN_IF_ERROR(path, litert::ErrorStatusBuilder::InvalidArgument())
+      << "path is null.";
+  *path = options->xnn.weight_cache_file_path;
+  return kLiteRtStatusOk;
+}
---
+++ litert/cc/options/litert_cpu_options_test.cc
@@ -0,0 +1,123 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "litert/cc/options/litert_cpu_options.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include "absl/strings/string_view.h"  // from @com_google_absl
+#include "litert/c/litert_common.h"
+#include "litert/c/litert_opaque_options.h"
+#include "litert/c/options/litert_cpu_options.h"
+#include "litert/cc/litert_expected.h"
+#include "litert/cc/litert_handle.h"
+#include "litert/cc/litert_macros.h"
+#include "litert/cc/litert_opaque_options.h"
+#include "litert/test/matchers.h"
+
+namespace litert {
+namespace {
+using testing::StrEq;
+using testing::litert::IsError;
+using testing::litert::IsOk;
+using testing::litert::IsOkAndHolds;
+
+// A test option implementation to discriminate against CpuOptions.
+struct NotCpuOptions : public OpaqueOptions {
+  using OpaqueOptions::OpaqueOptions;
+  static Expected<NotCpuOptions> Create() {
+    LiteRtOpaqueOptions options;
+    LITERT_RETURN_IF_ERROR(LiteRtCreateOpaqueOptions(
+        "test-option", new int,
+        [](void* i) { delete reinterpret_cast<int*>(i); }, &options));
+    return NotCpuOptions(options, OwnHandle::kYes);
+  }
+};
+
+TEST(CpuOptions, IdentifierIsCorrect) {
+  EXPECT_THAT(CpuOptions::Identifier(), StrEq(LiteRtGetCpuOptionsIdentifier()));
+}
+
+TEST(CpuOptions, CreateAndOwnedHandle) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+  EXPECT_TRUE(options.IsOwned());
+}
+
+TEST(CpuOptions, CreateFromAnotherCpuOptionsHandleWorks) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions original_options,
+                              CpuOptions::Create());
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options,
+                              CpuOptions::Create(original_options));
+  EXPECT_FALSE(options.IsOwned());
+  EXPECT_EQ(options.Get(), original_options.Get());
+}
+
+TEST(CpuOptions, CreateFromADifferentOptionFails) {
+  LITERT_ASSERT_OK_AND_ASSIGN(NotCpuOptions original_options,
+                              NotCpuOptions::Create());
+  OpaqueOptions& opaque_options = original_options;
+  EXPECT_THAT(CpuOptions::Create(opaque_options),
+              IsError(kLiteRtStatusErrorInvalidArgument));
+}
+
+TEST(CpuOptions, CheckNumThreadsDefaultValue) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+  EXPECT_THAT(options.GetNumThreads(), IsOkAndHolds(0));
+}
+
+TEST(CpuOptions, SetAndGetNumThreadsWorks) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+
+  LITERT_EXPECT_OK(options.SetNumThreads(5));
+  EXPECT_THAT(options.GetNumThreads(), IsOkAndHolds(5));
+}
+
+TEST(CpuOptions, CheckXNNPackWeightCachePathDefaultValue) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+  EXPECT_THAT(options.GetXNNPackWeightCachePath(),
+              IsOkAndHolds(absl::string_view()));
+}
+
+TEST(CpuOptions, SetAndGetXNNPackWeighCachePathWorks) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+
+  LITERT_EXPECT_OK(options.SetXNNPackWeightCachePath("a/path"));
+  EXPECT_THAT(options.GetXNNPackWeightCachePath(),
+              IsOkAndHolds(StrEq("a/path")));
+}
+
+TEST(CpuOptions, CheckXNNPackFlagsDefaultValue) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+  // Note: we can't check the default value for this as XNNPack compile options
+  // may affect it.
+  EXPECT_THAT(options.GetXNNPackFlags(), IsOk());
+}
+
+TEST(CpuOptions, SetAndGetXNNPackFlagsWorks) {
+  LITERT_ASSERT_OK_AND_ASSIGN(CpuOptions options, CpuOptions::Create());
+
+  LITERT_EXPECT_OK(options.SetXNNPackFlags(3));
+  EXPECT_THAT(options.GetXNNPackFlags(), IsOkAndHolds(3));
+}
+
+TEST(CpuOptions, GetXNNPackFlagsFailsIfErroneousCast) {
+  LITERT_ASSERT_OK_AND_ASSIGN(NotCpuOptions original_options,
+                              NotCpuOptions::Create());
+  OpaqueOptions& opaque_options = original_options;
+  CpuOptions& options = static_cast<CpuOptions&>(opaque_options);
+  EXPECT_THAT(options.GetXNNPackFlags(), IsError());
+}
+
+}  // namespace
+}  // namespace litert
