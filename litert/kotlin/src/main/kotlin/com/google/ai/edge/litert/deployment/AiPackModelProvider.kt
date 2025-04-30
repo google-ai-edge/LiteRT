70181157ac3d3ff09b7bf3e92ded3cd1dbb84d93
Byungchul Kim <byungchul@google.com>
2025-04-30 12:05:05 -0700

    Reverts bdafbf729dc1fd76983fc254740651d1ec5bde4b
    LiteRT-PiperOrigin-RevId: 753260582

--- litert/kotlin/src/main/kotlin/com/google/ai/edge/litert/deployment/AiPackModelProvider.kt
+++ litert/kotlin/src/main/kotlin/com/google/ai/edge/litert/deployment/AiPackModelProvider.kt
@@ -16,7 +16,7 @@
 
 package com.google.ai.edge.litert.deployment
 
-import android.app.Activity
+import android.content.Context
 import android.util.Log
 import com.google.ai.edge.litert.Accelerator
 import com.google.ai.edge.litert.ModelProvider
@@ -40,13 +40,13 @@ import kotlinx.coroutines.suspendCancellableCoroutine
  * NOTE: For install-time AiPack, use [ModelProvider.staticModel] instead.
  */
 class AiPackModelProvider(
-  private val activity: Activity,
+  private val context: Context,
   private val aiPackName: String,
   private val modelPath: String,
   private val accelerator: Accelerator,
   private vararg val moreAccelerators: Accelerator,
 ) : ModelProvider {
-  private val aiPackManager = AiPackManagerFactory.getInstance(activity)
+  private val aiPackManager = AiPackManagerFactory.getInstance(context)
 
   /** It's always a file, for on-demand AiPack. */
   override fun getType() = ModelProvider.Type.FILE
