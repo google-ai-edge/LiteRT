9ad0fea36d1d4d42c0b59f429885d663cd512f0b
Google AI Edge <ai-edge-bot@google.com>
2025-05-13 22:37:15 -0700

    Automated Code Change
    PiperOrigin-RevId: 758516909

--- tflite/delegates/coreml/builders/reshape_op_builder.cc
+++ tflite/delegates/coreml/builders/reshape_op_builder.cc
@@ -15,6 +15,9 @@ limitations under the License.
 #include "tflite/delegates/coreml/builders/reshape_op_builder.h"
 
 #include <algorithm>
+#include <cstdint>
+#include <cstring>
+#include <iterator>
 #include <memory>
 #include <string>
 
