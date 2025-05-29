f4a1f8a88f68511569bc90f7a9d3f8f4f93229ff
Google AI Edge <ai-edge-bot@google.com>
2025-05-28 21:54:21 -0700

    Internal change
    LiteRT-PiperOrigin-RevId: 764557683

--- litert/cc/litert_shared_library.cc
+++ litert/cc/litert_shared_library.cc
@@ -27,7 +27,7 @@
 #include <string>
 #include <utility>
 
-#include "absl/debugging/leak_check.h"  // from @com_google_absl
+#include "absl/debugging/leak_check.h"  // from @com_google_absl  // copybara:comment
 #include "absl/strings/str_format.h"  // from @com_google_absl
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "litert/c/litert_common.h"
@@ -139,7 +139,7 @@ Expected<SharedLibrary> SharedLibrary::LoadImpl(
       }
       lib.path_ = path;
       {
-        absl::LeakCheckDisabler disabler;
+        absl::LeakCheckDisabler disabler;  // copybara:comment
         lib.handle_ =
             dlopen(lib.Path().c_str(), SanitizeFlagsInCaseOfAsan(flags));
       }
