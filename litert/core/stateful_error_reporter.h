b55bb0b1e5f69c8d4a6c39fbb93005b6ce9ebbea
Google AI Edge <ai-edge-bot@google.com>
2025-07-20 20:48:33 -0700

    Adding basic Error reporter API
    Setting std error reporter as the default error reporter. And allow buffer error reporter to report more than one error
    
    LiteRT-PiperOrigin-RevId: 785271457

---
+++ litert/core/stateful_error_reporter.h
@@ -0,0 +1,36 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ODML_LITERT_LITERT_CORE_STATEFUL_ERROR_REPORTER_H_
+#define ODML_LITERT_LITERT_CORE_STATEFUL_ERROR_REPORTER_H_
+
+#include <string>
+
+#include "litert/core/error_reporter.h"
+
+namespace litert {
+
+// An ErrorReporter that keeps track of the last reported error.
+class StatefulErrorReporter : public ErrorReporter {
+ public:
+  // Inherit the variadic version of Report from the base class.
+  using ErrorReporter::Report;
+  
+  // Returns the last reported error message.
+  virtual std::string message() = 0;
+};
+
+}  // namespace litert
+
+#endif  // ODML_LITERT_LITERT_CORE_STATEFUL_ERROR_REPORTER_H_
