607f4026f9355d04b895669f345499c16e20e6b9
Gerardo Carranza <gcarranza@google.com>
2025-08-15 09:28:47 -0700

    Add Interpreter::AllocateTensors during CompiledModel InitializeRuntime
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google-ai-edge/LiteRT/pull/3130 from chuntl-qti:dev/chunhsue/qc_option_dispatch a7df82cfb003556379948ee49eab9727cca10a69
    LiteRT-PiperOrigin-RevId: 795503426

--- litert/runtime/compiled_model.cc
+++ litert/runtime/compiled_model.cc
@@ -46,7 +46,6 @@
 #include "litert/c/litert_tensor_buffer_requirements.h"
 #include "litert/c/litert_tensor_buffer_types.h"
 #include "litert/cc/litert_buffer_ref.h"
-#include "litert/cc/litert_event.h"
 #include "litert/cc/litert_expected.h"
 #include "litert/cc/litert_handle.h"
 #include "litert/cc/litert_macros.h"
@@ -153,6 +152,15 @@ Expected<void> LiteRtCompiledModelT::InitializeRuntime(
                       "Failed to build TFL interpreter");
   }
   interp_->SetNumThreads(num_threads);
+
+  if (auto res = interp_->AllocateTensors(); res != kTfLiteOk) {
+    if (error_reporter_) {
+      error_reporter_->Report(
+          "Failed to allocate tensors during runtime initialization");
+    }
+    return Unexpected(kLiteRtStatusErrorRuntimeFailure,
+                      "Failed to allocate tensors");
+  }
 
   if (profiler_ != nullptr) {
     interp_->SetProfiler(profiler_);
@@ -796,12 +804,8 @@ Expected<void> LiteRtCompiledModelT::Run(
     // synchronization events that have been attached to the outputs.
     for (auto& tb : output_buffers) {
       if (tb->HasEvent()) {
-        auto event = tb->GetEvent();
-        if (auto status = litert::Event(*event, litert::OwnHandle::kNo)
-                              .Wait(/*timeout_in_ms=*/-1);
-            !status) {
-          return status;
-        }
+        LITERT_ASSIGN_OR_RETURN(LiteRtEventT * event, tb->GetEvent());
+        LITERT_RETURN_IF_ERROR(event->Wait(/*timeout_in_ms=*/-1));
       }
     }
   }
