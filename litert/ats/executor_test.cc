531183a4be530495ce894bd51ac7db5823460807
Luke Boyer <lukeboyer@google.com>
2025-09-17 22:44:30 -0700

    Add timing and iters per test flags to ats. Simplify fuzz logic.
    LiteRT-PiperOrigin-RevId: 808409344

--- litert/ats/executor_test.cc
+++ litert/ats/executor_test.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "litert/cts/compiled_model_executor.h"
+#include "litert/ats/executor.h"
 
 #include <cstdint>
 #include <utility>
