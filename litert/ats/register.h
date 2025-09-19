5c1b9b0b0791208f6d503904e03d6d0e3aca7a96
Luke Boyer <lukeboyer@google.com>
2025-09-18 23:33:44 -0700

    Surface timeout check as function.
    LiteRT-PiperOrigin-RevId: 808897617

--- litert/ats/register.h
+++ litert/ats/register.h
@@ -16,41 +16,44 @@
 #define THIRD_PARTY_ODML_LITERT_LITERT_ATS_REGISTER_H_
 
 #include <cstddef>
-#include <cstdint>
+#include <memory>
+#include <utility>
 
-#include "absl/log/absl_check.h"  // from @com_google_absl
+#include "absl/strings/string_view.h"  // from @com_google_absl
 #include "litert/ats/configure.h"
-#include "litert/ats/executor.h"
+#include "litert/c/litert_common.h"
 #include "litert/c/litert_logging.h"
-#include "litert/c/litert_op_code.h"
 #include "litert/cc/litert_detail.h"
+#include "litert/cc/litert_expected.h"
+#include "litert/cc/litert_macros.h"
+#include "litert/cc/litert_model.h"
 #include "litert/cc/litert_rng.h"
+#include "litert/core/model/model_load.h"
 #include "litert/test/generators/generators.h"
-#include "tflite/schema/schema_generated.h"
+#include "litert/test/simple_buffer.h"
 
 namespace litert::testing {
 
+/// REGISTER GENERATED TESTS ///////////////////////////////////////////////////
+
 // Utility to register a test logic a given number of times with a common
 // random device.
-template <template <typename TestLogic, typename TestExecutor> typename Fixture>
+template <typename Fixture>
 class RegisterFunctor {
  public:
   template <typename Logic>
   void operator()() {
     DefaultDevice device(options_.GetSeedForParams(Logic::Name()));
     for (size_t i = 0; i < iters_; ++i) {
-      if (options_.Backend() == AtsConf::ExecutionBackend::kCpu) {
-        BuildParamsAndRegister<Fixture<Logic, CpuCompiledModelExecutor>>(device,
-                                                                         {});
-      } else if (options_.Backend() == AtsConf::ExecutionBackend::kGpu) {
-        ABSL_CHECK(false) << "GPU backend not supported yet.";
-      } else if (options_.Backend() == AtsConf::ExecutionBackend::kNpu) {
-        BuildParamsAndRegister<Fixture<Logic, NpuCompiledModelExecutor>>(
-            device, typename NpuCompiledModelExecutor::Args{
-                        options_.DispatchDir(),
-                        options_.PluginDir(),
-                    });
+      auto test_graph = Logic::Create(device);
+      if (!test_graph) {
+        LITERT_LOG(LITERT_WARNING, "Failed to create ATS test %lu, %s: %s",
+                   test_id_, Logic::Name().data(),
+                   test_graph.Error().Message().c_str());
+        continue;
       }
+      Fixture::Register(test_id_++, std::move(*test_graph), Logic::Name(),
+                        options_);
     }
   }
 
@@ -58,36 +61,6 @@ class RegisterFunctor {
       : iters_(iters), test_id_(test_id), options_(options) {}
 
  private:
-  template <typename TestClass, typename Device>
-  void BuildParamsAndRegister(Device& device,
-                              typename TestClass::Executor::Args&& args) {
-    auto params = TestClass::BuildSetupParams(device);
-    if (!params) {
-      LITERT_LOG(LITERT_WARNING,
-                 "Failed to build params for ATS test %lu, %s: %s", test_id_,
-                 TestClass::LogicName().data(),
-                 params.Error().Message().c_str());
-      return;
-    }
-
-    const auto suite_name = TestClass::FmtSuiteName(test_id_++);
-    const auto test_name = TestClass::FmtTestName(*params->model);
-
-    if (!options_.ShouldRegister(
-            absl::StrFormat("%s.%s", suite_name, test_name))) {
-      return;
-    }
-
-    if (auto status = TestClass::Register(
-            suite_name, test_name, std::move(*params), std::move(args),
-            options_.CreateDataBuilder(), options_.DataSeed());
-        !status) {
-      LITERT_LOG(LITERT_WARNING, "Failed to register ATS test %lu, %s: %s",
-                 test_id_, TestClass::LogicName().data(),
-                 status.Error().Message().c_str());
-    }
-  }
-
   const size_t iters_;
   size_t& test_id_;
   const AtsConf& options_;
@@ -97,47 +70,68 @@ class RegisterFunctor {
 // the given type lists and registers each specialization a given number
 // of times. Each of these registrations will yield a single test case with a
 // a different set of random parameters.
-template <template <typename TestLogic, typename TestExecutor> typename Fixture,
-          template <typename...> typename Logic, typename... Lists>
+template <typename Fixture, template <typename...> typename Logic,
+          typename... Lists>
 void RegisterCombinations(size_t iters, size_t& test_id,
                           const AtsConf& options) {
   RegisterFunctor<Fixture> f(iters, test_id, options);
   ExpandProduct<Logic, Lists...>(f);
 }
 
-// Helper aliases to set some of the template params that don't need to vary
-// for ats.
-template <typename Ranks, typename Types, typename OpCodes, typename Fas>
-using BinaryNoBroadcastAts =
-    BinaryNoBroadcast<Ranks, Types, OpCodes, Fas, SizeC<1024>>;
+/// REGISTER EXTRA MODELS TESTS ////////////////////////////////////////////////
 
-template <template <typename TestLogic, typename TestExecutor> typename Fixture>
-void RegisterAtsTests(const AtsConf& ats_options) {
-  size_t test_id = 0;
-  {
-    // NO OP //
-    // clang-format off
-    RegisterCombinations<
-        Fixture,
-        ExampleTestLogic,  // Test logic template
-        SizeListC<1, 2, 3, 4>,  // Ranks
-        TypeList<float, int32_t>  // Data types
-    >(/*iters=*/10, test_id, ats_options);
-    // clang-format on
+// Container for test graphs that originate from raw .tflite passed to the test.
+class ExtraModel : public TestGraph {
+ public:
+  using Ptr = std::unique_ptr<ExtraModel>;
+
+  static constexpr absl::string_view Name() { return "ExtraModel"; }
+
+  bool HasReference() const override { return false; }
+
+  static Expected<ExtraModel::Ptr> Create(absl::string_view model_path) {
+    LITERT_ASSIGN_OR_RETURN(auto model,
+                            internal::LoadModelFromFile(model_path));
+    return std::make_unique<ExtraModel>(std::move(model));
   }
 
-  {
-    // BINARY NO BCAST //
-    // clang-format off
-    RegisterCombinations<
-        Fixture,
-        BinaryNoBroadcastAts,  // Test logic template
-        SizeListC<1, 2, 3, 4, 5, 6>,  // Ranks
-        TypeList<float, int32_t>,  // Data types
-        OpCodeListC<kLiteRtOpCodeTflAdd, kLiteRtOpCodeTflSub>,  // Op codes
-        FaListC<::tflite::ActivationFunctionType_NONE>  // TODO: More support.
-    >(/*iters=*/10, test_id, ats_options);
-    // clang-format on
+  Expected<VarBuffers> MakeInputs(
+      DefaultDevice& device,
+      const RandomTensorDataBuilder& data_builder) const override {
+    VarBuffers inputs;
+    for (const auto& input : Graph().Subgraph(0).Inputs()) {
+      LITERT_ASSIGN_OR_RETURN(auto t, (input->Ranked()));
+      LITERT_ASSIGN_OR_RETURN(auto b,
+                              SimpleBuffer::Create(RankedTensorType(t)));
+      LITERT_RETURN_IF_ERROR((b.WriteRandom(data_builder, device)));
+      inputs.push_back(std::move(b));
+    }
+    return inputs;
+  }
+
+  Expected<void> Reference(const VarBuffers& inputs,
+                           VarBuffers& outputs) const override {
+    return Error(kLiteRtStatusErrorUnsupported);
+  }
+
+  explicit ExtraModel(LiteRtModelT::Ptr model) : TestGraph(std::move(model)) {}
+};
+
+// Registers a test for each extra model passed in the options.
+template <typename Fixture>
+void RegisterExtraModels(size_t& test_id, const AtsConf& options) {
+  DefaultDevice device(options.GetSeedForParams(ExtraModel::Name()));
+  const auto extra_models = options.ExtraModels();
+  LITERT_LOG(LITERT_INFO, "Registering %zu extra models", extra_models.size());
+  for (const auto& file : extra_models) {
+    auto model = ExtraModel::Create(file);
+    if (!model) {
+      LITERT_LOG(LITERT_WARNING, "Failed to create extra model %s: %s",
+                 file.c_str(), model.Error().Message().c_str());
+      continue;
+    }
+    Fixture::Register(test_id++, std::move(*model), ExtraModel::Name(), options,
+                      /*always_reg=*/true);
   }
 }
 
