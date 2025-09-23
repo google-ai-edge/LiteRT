38dd745b03ea923ed8c3d02054ab089b3d78935f
Chunlei Niu <niuchl@google.com>
2025-09-23 12:06:56 -0700

    Clean up `litert/c/options`' deps on types from `litert/cc`.
    LiteRT-PiperOrigin-RevId: 810531242

--- litert/c/options/litert_mediatek_options.cc
+++ litert/c/options/litert_mediatek_options.cc
@@ -18,11 +18,8 @@
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "litert/c/litert_common.h"
 #include "litert/c/litert_opaque_options.h"
-#include "litert/cc/litert_detail.h"
-#include "litert/cc/litert_expected.h"
-#include "litert/cc/litert_handle.h"
 #include "litert/cc/litert_macros.h"
-#include "litert/cc/litert_opaque_options.h"
+
 struct LiteRtMediatekOptionsT {
   LiteRtMediatekOptionsNeronSDKVersionType neron_sdk_version =
       kLiteRtMediatekOptionsNeronSDKVersionTypeVersion8;
