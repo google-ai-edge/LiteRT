333e342f1aec5b5b159efdb6f14f3298379c389d
Terry Heo <terryheo@google.com>
2025-09-26 10:45:09 -0700

    Add opaque_options dynamic runtime C++ API
    This API was missed and it is needed to fix ASAN errors
    on options_helper_test, litert_compiled_model_test.
    (To make sure the allocation and deallocation happens at the same library)
    
    LiteRT-PiperOrigin-RevId: 811875957

--- litert/cc/litert_opaque_options.h
+++ litert/cc/litert_opaque_options.h
@@ -22,6 +22,7 @@
 #include <type_traits>
 
 #include "absl/strings/string_view.h"  // from @com_google_absl
+#include "litert/c/litert_common.h"
 #include "litert/c/litert_opaque_options.h"
 #include "litert/cc/litert_expected.h"
 #include "litert/cc/litert_handle.h"
