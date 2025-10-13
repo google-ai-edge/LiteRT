9286ee544a11831a756c8e3827305495cc67eb68
weilhuan-qti <weilhuan@qti.qualcomm.com>
2025-10-13 17:15:14 +0800

    Use user-specified soc info to override online soc info.

--- litert/vendors/qualcomm/compiler/qnn_compiler_plugin_test.cc
+++ litert/vendors/qualcomm/compiler/qnn_compiler_plugin_test.cc
@@ -176,7 +176,7 @@ TEST(TestQnnPlugin, GetConfigInfo) {
   LiteRtParamIndex num_supported_soc_models;
   LITERT_ASSERT_OK(LiteRtGetNumCompilerPluginSupportedSocModels(
       plugin.get(), &num_supported_soc_models));
-  ASSERT_EQ(num_supported_soc_models, 9);
+  ASSERT_EQ(num_supported_soc_models, 10);
 
   const char* config_id;
   LITERT_ASSERT_OK(
