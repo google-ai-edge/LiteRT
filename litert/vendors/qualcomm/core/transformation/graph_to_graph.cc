8f2fb009ae43935ff35c704920747642f14c688a
jiunkaiy <jiunkaiy@qti.qualcomm.com>
2025-10-13 15:13:00 +0800

    Qualcomm AI Engine Direct - Optimize Attention block.
    Summary:
    - Enable MHA2SHA support for multiple attention blocks with shared masking operations.
    - Add unit tests to validate the new MHA2SHA functionality.

--- litert/vendors/qualcomm/core/transformation/graph_to_graph.cc
+++ litert/vendors/qualcomm/core/transformation/graph_to_graph.cc
@@ -179,5 +179,22 @@ void GraphToGraphTransform(const G2GConfig g2g_option,
   };
   Transform(validate_op_config, ops, tensor_pool, embedding_gemma,
             TransformEmbeddingGemma);
+
+  // Attention Optimization
+  const std::vector<QnnOpCode> attn = {
+      QnnOpCode::kElementWiseMultiply,
+      QnnOpCode::kElementWiseMultiply,
+      QnnOpCode::kTranspose,
+      QnnOpCode::kTranspose,
+      QnnOpCode::kMatMul,
+      QnnOpCode::kReshape,
+      QnnOpCode::kElementWiseBinary,
+      QnnOpCode::kElementWiseSelect,
+      QnnOpCode::kSoftmax,
+      QnnOpCode::kTranspose,
+      QnnOpCode::kMatMul,
+      QnnOpCode::kTranspose,
+  };
+  Transform(validate_op_config, ops, tensor_pool, attn, OptimizeMHAAttn);
 }
 }  // namespace qnn
