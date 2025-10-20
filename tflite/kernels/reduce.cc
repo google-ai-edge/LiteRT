0445833357da202a244ba6a35ffb5160e8ef43f8
Souhil Louddad <louddadsouhilabdelmalek@gmail.com>
2025-10-20 10:45:59 -0700

    PR #102554: TFLite: Fix ReduceSum reference kernel overflow by using GetTensorDat…
    Imported from GitHub PR https://github.com/tensorflow/tensorflow/pull/102554
    
    …a<T> instead of uint8_t as he mentioned.
    Copybara import of the project:
    
    --
    8f8a8b1804691d6dbd456bb93446fd65706eaf6b by Souhil <louddadsouhilabdelmalek@gmail.com>:
    
    TFLite: Fix ReduceSum reference kernel overflow by using GetTensorData<T> instead of uint8_t as he mentioned.
    
    --
    93488c2327bb7244aa379e4a45e5e2bea33c2c11 by Souhil <louddadsouhilabdelmalek@gmail.com>:
    
    Add test for ReduceSum overflow fix in reference kernel
    
    --
    f2fe493202b6f80d1a503d7d32e4aa8d8ad0dbca by Souhil <louddadsouhilabdelmalek@gmail.com>:
    
    Add test for ReduceSum overflow fix in reference kernel
    
    Merging this change closes #102554
    
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/tensorflow/tensorflow/pull/102554 from souhil25:fix/reduce-overflow f2fe493202b6f80d1a503d7d32e4aa8d8ad0dbca
    PiperOrigin-RevId: 821712557

--- tflite/kernels/reduce.cc
+++ tflite/kernels/reduce.cc
@@ -477,16 +477,15 @@ TfLiteStatus QuantizedMeanOrSum(TfLiteContext* context,
     TF_LITE_ENSURE(
         context,
         reference_ops::QuantizedMeanOrSum(
-            GetTensorData<uint8_t>(op_context.input),
+            GetTensorData<T>(op_context.input),
             op_context.input->params.zero_point, op_context.input->dims->data,
-            op_context.input->dims->size,
-            GetTensorData<uint8_t>(op_context.output), op_data->multiplier,
-            op_data->shift, op_context.output->params.zero_point,
-            op_context.output->dims->data, op_context.output->dims->size,
-            GetTensorData<int>(op_context.axis), num_axis,
-            op_context.params->keep_dims, GetTensorData<int>(temp_index),
-            GetTensorData<int>(resolved_axis), GetTensorData<int32_t>(temp_sum),
-            compute_sum));
+            op_context.input->dims->size, GetTensorData<T>(op_context.output),
+            op_data->multiplier, op_data->shift,
+            op_context.output->params.zero_point, op_context.output->dims->data,
+            op_context.output->dims->size, GetTensorData<int>(op_context.axis),
+            num_axis, op_context.params->keep_dims,
+            GetTensorData<int>(temp_index), GetTensorData<int>(resolved_axis),
+            GetTensorData<int32_t>(temp_sum), compute_sum));
   }
   return kTfLiteOk;
 }
