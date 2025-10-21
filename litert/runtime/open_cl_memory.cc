06863cf321eb5c32b625391968e32974e7bc7590
Google AI Edge <ai-edge-bot@google.com>
2025-10-20 22:05:20 -0700

    Automated Code Change
    LiteRT-PiperOrigin-RevId: 821946927

--- litert/runtime/open_cl_memory.cc
+++ litert/runtime/open_cl_memory.cc
@@ -54,7 +54,7 @@ template Expected<void> OpenClMemory::Unlock<char>();
 
 template <typename T>
 Expected<T*> OpenClMemory::Lock(LiteRtTensorBufferLockMode mode) {
-  absl::MutexLock lock(&mutex_);
+  absl::MutexLock lock(mutex_);
   LITERT_RETURN_IF_ERROR(
       IsSupported(),
       Unexpected(kLiteRtStatusErrorRuntimeFailure, "OpenCL is not supported"));
@@ -107,7 +107,7 @@ Expected<T*> OpenClMemory::Lock(LiteRtTensorBufferLockMode mode) {
 
 template <typename T>
 Expected<void> OpenClMemory::Unlock() {
-  absl::MutexLock lock(&mutex_);
+  absl::MutexLock lock(mutex_);
   LITERT_RETURN_IF_ERROR(
       IsSupported(),
       Unexpected(kLiteRtStatusErrorRuntimeFailure, "OpenCL is not supported"));
