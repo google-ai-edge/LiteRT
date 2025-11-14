68c99f467750548b6014279ab17eb11b8fb4bf0f
Steven Toribio <toribiosteven@google.com>
2025-11-14 13:41:09 -0800

    Add GroupNorm op builder for Qualcomm.
    This change introduces a builder for the `QNN_OP_GROUP_NORM` operation. It includes:
    -   Adding `odml.group_norm` to the LiteRtOpOptions.
    -   Implementing `BuildGroupNormOp` to construct the Qualcomm GroupNorm operation with epsilon and number of groups.
    
    LiteRT-PiperOrigin-RevId: 832441752

--- litert/vendors/qualcomm/compiler/qnn_compiler_plugin_test.cc
+++ litert/vendors/qualcomm/compiler/qnn_compiler_plugin_test.cc
@@ -138,6 +138,7 @@ const auto kSupportedOps =
                     "simple_prelu_op.tflite",
                     "simple_l2_norm.tflite",
                     "l2_norm_composite.tflite",
+                    "simple_group_norm_op.tflite",
                     kFeedForwardModel,
                     kKeyEinsumModel,
                     kQueryEinsumModel,
