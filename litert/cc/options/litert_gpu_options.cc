bbb0ce0bed7d5890e2b82f08892c28b7eaaafc20
Fengwu Yao <fengwuyao@google.com>
2025-12-02 15:59:24 -0800

    Add option to use Metal argument buffers in LiteRt GPU options.
    LiteRT-PiperOrigin-RevId: 839460656

--- litert/cc/options/litert_gpu_options.cc
+++ litert/cc/options/litert_gpu_options.cc
@@ -122,4 +122,12 @@ LiteRtStatus GpuOptions::SetNumStepsOfCommandBufferPreparations(
           Get(), num_steps_of_command_buffer_preparations);
 }
 
+#ifdef __APPLE__
+LiteRtStatus GpuOptions::SetUseMetalArgumentBuffers(
+    bool use_metal_argument_buffers) {
+  return LiteRtSetGpuOptionsUseMetalArgumentBuffers(Get(),
+                                                    use_metal_argument_buffers);
+}
+#endif  // __APPLE__
+
 }  // namespace litert
--- litert/cc/options/litert_gpu_options.h
+++ litert/cc/options/litert_gpu_options.h
@@ -87,6 +87,12 @@ class GpuOptions : public litert::OpaqueOptions {
   //     input and output KV caches.
   LiteRtStatus SetNumStepsOfCommandBufferPreparations(
       int num_steps_of_command_buffer_preparations);
+
+#ifdef __APPLE__
+  // Sets whether to use Metal argument buffers. WARNING: This is only
+  // applicable to Metal backend.
+  LiteRtStatus SetUseMetalArgumentBuffers(bool use_metal_argument_buffers);
+#endif  // __APPLE__
 };
 
 }  // namespace litert
