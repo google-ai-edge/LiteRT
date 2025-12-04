8b54326a5773b80278eea5894a8f75c849883382
chunhsue-qti <chunhsue@qti.qualcomm.com>
2025-12-04 09:21:51 +0800

    Qualcomm AI Engine Direct - Add QNN E-wise Max & Div INT16 tests.
    Co-Authored-By: William Lin <chengwl@qti.qualcomm.com>

--- litert/vendors/qualcomm/core/utils/miscs.h
+++ litert/vendors/qualcomm/core/utils/miscs.h
@@ -42,6 +42,14 @@ float Dequantize(const T val, const float scale, const int32_t zero_point) {
   return scale * (val - zero_point);
 }
 
+template <typename T>
+void DequantizeInto(const absl::Span<const T>& in, const float s,
+                    const int32_t zp, std::vector<float>& out) {
+  for (size_t i = 0; i < in.size(); ++i) {
+    out.emplace_back(Dequantize(in[i], s, zp));
+  }
+}
+
 void ConvertDataFromInt16toUInt16(absl::Span<const std::int16_t> src,
                                   std::vector<std::uint16_t>& dst);
 
