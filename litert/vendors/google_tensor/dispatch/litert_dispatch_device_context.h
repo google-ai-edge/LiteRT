1e32481e20ecfc0c0581e1deeefdb93fe9e5c5df
Google AI Edge <ai-edge-bot@google.com>
2026-01-16 11:24:11 -0800

    Google Tensor: only use indexed node binding for Graph vendor Dispatch API
    LiteRT-PiperOrigin-RevId: 857237173

--- litert/vendors/google_tensor/dispatch/litert_dispatch_device_context.h
+++ litert/vendors/google_tensor/dispatch/litert_dispatch_device_context.h
@@ -16,44 +16,19 @@
 #define ODML_LITERT_LITERT_VENDORS_GOOGLE_TENSOR_DISPATCH_LITERT_DISPATCH_DEVICE_CONTEXT_H_
 
 #include <cstdint>
-#include <memory>
 #include <optional>
 #include <utility>
 
+#include "absl/base/nullability.h"  // from @com_google_absl
 #include "absl/container/flat_hash_set.h"  // from @com_google_absl
 #include "litert/c/litert_common.h"
 #include "litert/vendors/c/litert_dispatch.h"
 #include "litert/vendors/google_tensor/dispatch/sb_api.h"
 
+// This class is thread-compatible.
 class LiteRtDispatchDeviceContextT {
  public:
-  static LiteRtStatus Create(LiteRtDispatchDeviceContext& device_context);
-
-  ~LiteRtDispatchDeviceContextT();
-
-  LiteRtStatus RegisterTensorBuffer(
-      LiteRtTensorBuffer tensor_buffer,
-      LiteRtTensorBufferHandle& tensor_buffer_handle);
-
-  LiteRtStatus UnregisterTensorBuffer(
-      LiteRtTensorBufferHandle tensor_buffer_handle);
-
-  LiteRtStatus CreateGraph(LiteRtDispatchGraph& graph);
-
-  LiteRtStatus DestroyGraph(LiteRtDispatchGraph graph);
-
-  LiteRtStatus LoadExecutable(LiteRtDispatchExecutableType type,
-                              const LiteRtMemBuffer& bytecode_buffer,
-                              LiteRtDispatchExecutableHandle& exec_handle);
-
-  LiteRtStatus UnloadExecutable(LiteRtDispatchExecutableHandle exec_handle);
-
-  ThrContext* thr_context() { return thr_context_.get(); }
-
-  void add_graph(ThrGraph* graph) { thr_graphs_.insert(graph); }
-
- private:
-  // Struct to store DarwiNN options for later application.
+  // Stores DarwiNN options for later annotation.
   struct DarwinnOptionsData {
     std::optional<uint32_t> inference_power_state;
     std::optional<uint32_t> inference_memory_power_state;
@@ -62,20 +37,54 @@ class LiteRtDispatchDeviceContextT {
     bool prefer_coherent = false;
   };
 
-  static void ThrContextDeleter(ThrContext* thr_context);
+  static LiteRtStatus Create(LiteRtDispatchDeviceContext& device_context);
 
-  using ThrContextPtr = std::unique_ptr<ThrContext,
-                                        decltype(&ThrContextDeleter)>;
+  LiteRtStatus Destroy();
 
+  LiteRtStatus RegisterTensorBuffer(
+      LiteRtTensorBuffer tensor_buffer,
+      LiteRtTensorBufferHandle& tensor_buffer_handle);
+
+  LiteRtStatus UnregisterTensorBuffer(
+      LiteRtTensorBufferHandle tensor_buffer_handle);
+
+  LiteRtStatus LoadExecutable(LiteRtDispatchExecutableType type,
+                              const LiteRtMemBuffer& bytecode_buffer,
+                              LiteRtDispatchExecutableHandle& exec_handle);
+
+  LiteRtStatus UnloadExecutable(LiteRtDispatchExecutableHandle exec_handle);
+
+  // Registers a graph with the device context.
+  //
+  // This has the effect of guaranteeing that the device context remains alive
+  // until the graph is unregistered, meaning that a subsequent call to
+  // `UnregisterGraph` is required to permit the device context to be destroyed.
+  //
+  // NOTE: a graph may only be registered once.
+  LiteRtStatus RegisterGraph(LiteRtDispatchGraph graph);
+
+  LiteRtStatus UnregisterGraph(LiteRtDispatchGraph graph);
+
+  ThrContext* absl_nonnull thr_context() { return thr_context_; }
+
+  const std::optional<DarwinnOptionsData>& darwinn_options() {
+    return darwinn_options_;
+  }
+
+ private:
   LiteRtDispatchDeviceContextT(
-      ThrContextPtr thr_context,
+      ThrContext* absl_nonnull thr_context,
       std::optional<DarwinnOptionsData> darwinn_options)
-      : thr_context_(std::move(thr_context)),
+      : thr_context_(thr_context),
         darwinn_options_(std::move(darwinn_options)) {}
 
-  ThrContextPtr thr_context_;
+  // Consumers of this class must use `Destroy` to delete the instance.
+  ~LiteRtDispatchDeviceContextT() = default;
+
+  ThrContext* absl_nonnull thr_context_;
   std::optional<DarwinnOptionsData> darwinn_options_;
-  absl::flat_hash_set<ThrGraph*> thr_graphs_;
+  // A device context cannot be destroyed with any registered graphs.
+  absl::flat_hash_set<LiteRtDispatchGraph> registered_graphs_;
 };
 
 #endif  // ODML_LITERT_LITERT_VENDORS_GOOGLE_TENSOR_DISPATCH_LITERT_DISPATCH_DEVICE_CONTEXT_H_
--- litert/vendors/google_tensor/dispatch/litert_dispatch_graph.cc
+++ litert/vendors/google_tensor/dispatch/litert_dispatch_graph.cc
@@ -15,35 +15,29 @@
 #include "litert/vendors/google_tensor/dispatch/litert_dispatch_graph.h"
 
 #include <cinttypes>
+#include <cstdint>
+#include <optional>
+#include <string>
+#include <utility>
 
 #include "absl/base/nullability.h"  // from @com_google_absl
+#include "absl/cleanup/cleanup.h"  // from @com_google_absl
 #include "litert/c/internal/litert_logging.h"
 #include "litert/c/litert_common.h"
+#include "litert/cc/litert_macros.h"
 #include "litert/vendors/c/litert_dispatch.h"
 #include "litert/vendors/google_tensor/dispatch/dispatch_api_macros.h"
 #include "litert/vendors/google_tensor/dispatch/dispatch_api_utils.h"
 #include "litert/vendors/google_tensor/dispatch/litert_dispatch_device_context.h"
 #include "litert/vendors/google_tensor/dispatch/sb_api.h"
+#include "litert/vendors/google_tensor/dispatch/sb_dispatch_annotations.h"
 
 namespace gt = litert::google_tensor;
 
-LiteRtStatus LiteRtDispatchGraphT::Create(
-    LiteRtDispatchDeviceContext device_context, LiteRtDispatchGraph& graph) {
-  GT_LOG_RETURN_IF_NULL(device_context);
+namespace {
 
-  ThrGraph* thr_graph = thrGraphCreate(device_context->thr_context());
-  if (thr_graph == nullptr) {
-    LITERT_LOG(LITERT_ERROR, "Failed to create SB graph");
-    return kLiteRtStatusErrorRuntimeFailure;
-  }
-
-  graph = new LiteRtDispatchGraphT(device_context, thr_graph);
-  return kLiteRtStatusOk;
-}
-
-LiteRtStatus LiteRtDispatchGraphT::AddNode(LiteRtDispatchNodeId node_id,
-                                           LiteRtDispatchNodeType node_type) {
-  ThrNodeType thr_node_type;
+LiteRtStatus ToThrNodeType(LiteRtDispatchNodeType node_type,
+                           ThrNodeType& thr_node_type) {
   switch (node_type) {
     case kLiteRtDispatchNodeTypeDsp:
       thr_node_type = kThrNodeTypeDsp;
@@ -56,13 +50,162 @@ LiteRtStatus LiteRtDispatchGraphT::AddNode(LiteRtDispatchNodeId node_id,
       return kLiteRtStatusErrorInvalidArgument;
   }
 
+  return kLiteRtStatusOk;
+}
+
+}  // namespace
+
+LiteRtStatus LiteRtDispatchGraphT::Create(
+    LiteRtDispatchDeviceContext device_context, LiteRtDispatchGraph& graph) {
+  GT_LOG_RETURN_IF_NULL(device_context);
+
+  ThrGraph* thr_graph = thrGraphCreate(device_context->thr_context());
+  if (thr_graph == nullptr) {
+    LITERT_LOG(LITERT_ERROR, "Failed to create SB graph");
+    return kLiteRtStatusErrorRuntimeFailure;
+  }
+
+  // The returned instance must be allocated with `new`, as it will be
+  // deallocated via `delete` in `Destroy`.
+  graph = new LiteRtDispatchGraphT(device_context, thr_graph);
+  absl::Cleanup graph_cleanup = [graph] { graph->Destroy(); };
+
+  if (device_context->darwinn_options().has_value()) {
+    const LiteRtDispatchDeviceContextT::DarwinnOptionsData& darwinn_options =
+        *(device_context->darwinn_options());
+
+    if (std::optional<uint32_t> device_power_state =
+            darwinn_options.inference_power_state;
+        device_power_state.has_value()) {
+      LITERT_RETURN_IF_ERROR(graph->AnnotateGraph(
+          gt::DispatchDirectiveAnnotations::kEdgetpuDevicePowerState.data(),
+          std::to_string(*device_power_state).c_str()));
+    }
+
+    if (std::optional<uint32_t> memory_power_state =
+            darwinn_options.inference_memory_power_state;
+        memory_power_state.has_value()) {
+      LITERT_RETURN_IF_ERROR(graph->AnnotateGraph(
+          gt::DispatchDirectiveAnnotations::kEdgetpuMemoryPowerState.data(),
+          std::to_string(*memory_power_state).c_str()));
+    }
+
+    if (std::optional<int8_t> inference_priority =
+            darwinn_options.inference_priority;
+        inference_priority.has_value()) {
+      LITERT_RETURN_IF_ERROR(graph->AnnotateGraph(
+          gt::DispatchDirectiveAnnotations::kPriority.data(),
+          std::to_string(*inference_priority).c_str()));
+    }
+
+    if (darwinn_options.atomic_inference) {
+      LITERT_RETURN_IF_ERROR(graph->AnnotateGraph(
+          gt::DispatchDirectiveAnnotations::kEdgetpuAtomicInference.data(),
+          "1"));
+    }
+
+    if (darwinn_options.prefer_coherent) {
+      LITERT_RETURN_IF_ERROR(graph->AnnotateGraph(
+          gt::GraphDirectiveAnnotations::kPreferCoherent.data(), "1"));
+    }
+
+    LITERT_LOG(LITERT_INFO,
+               "Successfully applied Darwinn options as graph annotations");
+  }
+
+  LITERT_RETURN_IF_ERROR(device_context->RegisterGraph(graph));
+
+  std::move(graph_cleanup).Cancel();
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::Destroy() {
+  LITERT_RETURN_IF_ERROR(device_context_->UnregisterGraph(this));
+
+  GT_LOG_RETURN_IF_SB_ERROR(thrGraphDelete(thr_graph_),
+                            "Failed to delete SB graph");
+
+  delete this;
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::AddPositionalNode(
+    LiteRtDispatchNodeId node_id, LiteRtDispatchNodeType node_type) {
+  ThrNodeType thr_node_type;
+  LITERT_RETURN_IF_ERROR(ToThrNodeType(node_type, thr_node_type));
+
+  GT_LOG_RETURN_IF_SB_ERROR(
+      thrGraphAddSqNode(thr_graph_, gt::ToThrNodeId(node_id), thr_node_type),
+      "Failed to add positional node %" PRIu64 " with type %d to SB graph",
+      node_id, node_type);
+
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::ConnectPositionalNodeInput(
+    LiteRtDispatchNodeId node_id, LiteRtDispatchEdgeId edge_id) {
+  GT_LOG_RETURN_IF_SB_ERROR(
+      thrGraphConnectNodeInput(thr_graph_, gt::ToThrNodeId(node_id),
+                               gt::ToThrEdgeId(edge_id)),
+      "Failed to connect positional node %" PRIu64 " to input edge %" PRIu64,
+      node_id, edge_id);
+
+  input_edge_ids_.insert_or_assign(input_edge_ids_.size(), edge_id);
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::ConnectPositionalNodeOutput(
+    LiteRtDispatchNodeId node_id, LiteRtDispatchEdgeId edge_id) {
+  GT_LOG_RETURN_IF_SB_ERROR(
+      thrGraphConnectNodeOutput(thr_graph_, gt::ToThrNodeId(node_id),
+                                gt::ToThrEdgeId(edge_id)),
+      "Failed to connect positional node %" PRIu64 " to output edge %" PRIu64,
+      node_id, edge_id);
+
+  output_edge_ids_.insert_or_assign(output_edge_ids_.size(), edge_id);
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::AddIndexedNode(
+    LiteRtDispatchNodeId node_id, LiteRtDispatchNodeType node_type) {
+  ThrNodeType thr_node_type;
+  LITERT_RETURN_IF_ERROR(ToThrNodeType(node_type, thr_node_type));
+
   GT_LOG_RETURN_IF_SB_ERROR(
       thrGraphAddSqNodeWithInterfaceBindingMode(
           thr_graph_, gt::ToThrNodeId(node_id), thr_node_type,
           kThrNodeInterfaceBindingModeIndexed),
-      "Failed to add node %" PRIu64 " with type %d to SB graph", node_id,
-      node_type);
+      "Failed to add indexed node %" PRIu64 " with type %d to SB graph",
+      node_id, node_type);
 
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::ConnectIndexedNodeInput(
+    LiteRtDispatchNodeId node_id, int input_index,
+    LiteRtDispatchEdgeId edge_id) {
+  GT_LOG_RETURN_IF_SB_ERROR(thrGraphConnectNodeInputWithPortIndex(
+                                thr_graph_, gt::ToThrNodeId(node_id),
+                                gt::ToThrEdgeId(edge_id), input_index),
+                            "Failed to connect indexed node %" PRIu64
+                            " to input edge %" PRIu64 " at index %d",
+                            node_id, edge_id, input_index);
+
+  input_edge_ids_.insert_or_assign(input_index, edge_id);
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::ConnectIndexedNodeOutput(
+    LiteRtDispatchNodeId node_id, int output_index,
+    LiteRtDispatchEdgeId edge_id) {
+  GT_LOG_RETURN_IF_SB_ERROR(thrGraphConnectNodeOutputWithPortIndex(
+                                thr_graph_, gt::ToThrNodeId(node_id),
+                                gt::ToThrEdgeId(edge_id), output_index),
+                            "Failed to connect indexed node %" PRIu64
+                            " to output edge %" PRIu64 " at index %d",
+                            node_id, edge_id, output_index);
+
+  output_edge_ids_.insert_or_assign(output_index, edge_id);
   return kLiteRtStatusOk;
 }
 
@@ -71,34 +214,6 @@ LiteRtStatus LiteRtDispatchGraphT::AddEdge(LiteRtDispatchEdgeId edge_id) {
       thrGraphAddEdge(thr_graph_, gt::ToThrEdgeId(edge_id), kThrEdgeTypeTensor),
       "Failed to add edge %" PRIu64 " to SB graph", edge_id);
 
-  return kLiteRtStatusOk;
-}
-
-LiteRtStatus LiteRtDispatchGraphT::ConnectNodeInput(
-    LiteRtDispatchNodeId node_id, int input_index,
-    LiteRtDispatchEdgeId edge_id) {
-  GT_LOG_RETURN_IF_SB_ERROR(thrGraphConnectNodeInputWithPortIndex(
-                                thr_graph_, gt::ToThrNodeId(node_id),
-                                gt::ToThrEdgeId(edge_id), input_index),
-                            "Failed to connect node %" PRIu64
-                            " to input edge %" PRIu64 " at index %d",
-                            node_id, edge_id, input_index);
-
-  AddInputEdge(input_index, edge_id);
-  return kLiteRtStatusOk;
-}
-
-LiteRtStatus LiteRtDispatchGraphT::ConnectNodeOutput(
-    LiteRtDispatchNodeId node_id, int output_index,
-    LiteRtDispatchEdgeId edge_id) {
-  GT_LOG_RETURN_IF_SB_ERROR(thrGraphConnectNodeOutputWithPortIndex(
-                                thr_graph_, gt::ToThrNodeId(node_id),
-                                gt::ToThrEdgeId(edge_id), output_index),
-                            "Failed to connect node %" PRIu64
-                            " to output edge %" PRIu64 " at index %d",
-                            node_id, edge_id, output_index);
-
-  AddOutputEdge(output_index, edge_id);
   return kLiteRtStatusOk;
 }
 
@@ -164,6 +279,29 @@ LiteRtStatus LiteRtDispatchGraphT::AnnotateEdge(
       thrGraphAnnotateEdge(thr_graph_, gt::ToThrEdgeId(edge_id), key, value),
       "Failed to annotate edge %" PRIu64 " with key '%s' and value '%s'",
       edge_id, key, value);
+
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::RegisterInvocationContext(
+    LiteRtDispatchInvocationContext icontext) {
+  if (auto [_, inserted] = registered_icontexts_.insert(icontext); !inserted) {
+    LITERT_LOG(LITERT_ERROR, "Invocation context 0x%p is already registered",
+               icontext);
+    return kLiteRtStatusErrorInvalidArgument;
+  }
+
+  return kLiteRtStatusOk;
+}
+
+LiteRtStatus LiteRtDispatchGraphT::UnregisterInvocationContext(
+    LiteRtDispatchInvocationContext icontext) {
+  if (registered_icontexts_.erase(icontext) == 0) {
+    LITERT_LOG(LITERT_ERROR,
+               "Invocation context 0x%p was not previously registered",
+               icontext);
+    return kLiteRtStatusErrorInvalidArgument;
+  }
 
   return kLiteRtStatusOk;
 }
