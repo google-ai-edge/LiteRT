5193052f319c1343d5297649f85cfa475f694dce
Google AI Edge <ai-edge-bot@google.com>
2026-02-02 21:10:54 -0800

    Reverts 74b184b35d4f10ae116e9e790ed1c259b762d9ea
    LiteRT-PiperOrigin-RevId: 864669851

--- litert/cc/litert_tensor_buffer.h
+++ litert/cc/litert_tensor_buffer.h
@@ -114,8 +114,8 @@ class TensorBuffer : public internal::BaseHandle<LiteRtTensorBuffer> {
       TensorBufferType buffer_type, LiteRtWGPUBuffer buffer, size_t size_bytes);
 
   static Expected<TensorBuffer> CreateFromWebGpuTexture(
-      const Environment& env, const RankedTensorType& tensor_type,
-      void* texture, size_t size_bytes);
+      LiteRtEnvironment env, const RankedTensorType& tensor_type, void* texture,
+      size_t size_bytes);
 #endif  // LITERT_HAS_WEBGPU_SUPPORT
 
 #if LITERT_HAS_METAL_SUPPORT
