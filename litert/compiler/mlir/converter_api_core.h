ed206f360beab305cd8e13eb731803f6ebb53d1b
Chun-nien Chan <cnchan@google.com>
2026-02-05 16:29:28 -0800

    update converter-next apis for litert-torch
    LiteRT-PiperOrigin-RevId: 866164072

--- litert/compiler/mlir/converter_api_core.h
+++ litert/compiler/mlir/converter_api_core.h
@@ -21,17 +21,47 @@
 #include "absl/status/statusor.h"  // from @com_google_absl
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "absl/types/span.h"  // from @com_google_absl
+#include "llvm/ADT/SmallVector.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/IR/MLIRContext.h"
 #include "mlir/IR/OwningOpRef.h"
+#include "mlir/Pass/PassManager.h"
 
 namespace litert {
+
+// Config for the conversion from StableHLO to TFL. This config would be
+// translated to the `tflite::ConverterFlags` and `mlir::TFL::PassConfig` in the
+// TFL converter.
+struct ConvertToTFLConfig {
+  // The source model type.
+  std::string model_origin_framework = "UNSET";
+
+  // When set to true, convert +Inf/-Inf to MIN/MAX float value and output of
+  // convert only contains finite values.
+  bool canonicalizing_inf_as_min_max_float = true;
+
+  // Whether to consider this model a quantized model with quantize/dequantize
+  // ops and to convert kernels to quantized kernels wherever appropriate.
+  // WARNING: Experimental interface, subject to change.
+  std::string qdq_conversion_mode = "NONE";
+
+  // When set to true, allows fusion of dynamic shaped broadcast ops. It helps
+  // fusing implicit broadcasting ops when output shape has dynamic dimensions,
+  // but it may cause incorrect results when broadcasting ops are introduced by
+  // explicit broadcasting in the source model.
+  bool unsafe_fuse_dynamic_shaped_broadcast = false;
+};
+
+// Register passes to be used via MLIR pybindings pass manager.
+void RegisterPasses();
 
 // Config and register the dialects and passes for MLIR context.
 void PrepareMlirContext(mlir::MLIRContext* context);
 
-// Runs all passes from StableHLO to TFL (the default conversion path).
-absl::Status RunStableHLOToTFLPasses(mlir::ModuleOp module);
+// Runs all passes from TF/StableHLO to TFL (the default conversion path).
+absl::Status RunConvertToTFLPasses(mlir::ModuleOp module,
+                                   mlir::PassManager& pass_manager,
+                                   const ConvertToTFLConfig& config);
 
 // Sets the flatbuffer signature for the given MLIR operation. The operation
 // can be a func.FuncOp or a ModuleOp (when there is a func.FuncOp named
@@ -49,6 +79,10 @@ absl::StatusOr<mlir::OwningOpRef<mlir::ModuleOp>> MergeModuleOps(
 // path.
 absl::Status ExportFlatbufferToFile(mlir::ModuleOp module_op,
                                     absl::string_view output_path);
+
+// Exports the MLIR module to flatbuffer and returns the bytes.
+absl::StatusOr<llvm::SmallVector<char>> ExportFlatbufferToBytes(
+    mlir::ModuleOp module_op);
 
 }  // namespace litert
 
