0edfc0d09a3ea9e28c81c25f3a91640ead8e951a
Alexander Shaposhnikov <ashaposhnikov@google.com>
2026-02-12 18:36:24 -0800

    Update XNNPACK version.
    PiperOrigin-RevId: 869476659

--- tflite/tools/cmake/modules/cpuinfo.cmake
+++ tflite/tools/cmake/modules/cpuinfo.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   cpuinfo
   GIT_REPOSITORY https://github.com/pytorch/cpuinfo
   # Sync with tensorflow/workspace2.bzl
-  GIT_TAG 8a9210069b5a37dd89ed118a783945502a30a4ae
+  GIT_TAG f9a03241f8c3d4ed0c9728f5d70bff873d43d4e0
   GIT_PROGRESS TRUE
   SOURCE_DIR "${CMAKE_BINARY_DIR}/cpuinfo"
 )
