995903fe1e4f1b8907c69d7ff797c44000900819
Terry Heo <terryheo@google.com>
2026-02-20 16:03:39 -0800

    Use TOML for GpuOptions
    - Make GpuOptions no longer use runtime opaque type LiteRtGpuOptionsPayloadT.
      Instead it uses a concrete type LrtGpuOptions.
    - When it passes to runtime, it's serialized into TOML string using
      LrtGetOpaqueCpuOptionsData() API.
    - Changed C API prefix to "Lrt" since they're no longer part of libLiteRt.so
    - All fields are std::optional to only serialize those options user provided.
    - LrtGetXXX() returns default value via std::options::value_or().
    - Unlike CPU Options, GPU Options don't have consumer side data type.
      Instead, consumers use C APIs which return default values when they're not set.
    
    LiteRT-PiperOrigin-RevId: 873111811

--- litert/cc/options/litert_gpu_options.cc
+++ litert/cc/options/litert_gpu_options.cc
@@ -16,175 +16,173 @@
 
 #include "litert/c/litert_common.h"
 #include "litert/c/options/litert_gpu_options.h"
-#include "litert/cc/internal/litert_handle.h"
 #include "litert/cc/litert_expected.h"
 #include "litert/cc/litert_macros.h"
 
 namespace litert {
 
 const char* GpuOptions::GetPayloadIdentifier() {
-  return LiteRtGetGpuOptionsPayloadIdentifier();
+  return LrtGetGpuOptionsIdentifier();
 }
 
 Expected<GpuOptions> GpuOptions::Create() {
-  LiteRtOpaqueOptions options;
-  LITERT_RETURN_IF_ERROR(LiteRtCreateGpuOptions(&options));
-  return GpuOptions(options, OwnHandle::kYes);
+  LrtGpuOptions* options;
+  LITERT_RETURN_IF_ERROR(LrtCreateGpuOptions(&options));
+  return GpuOptions(options);
 }
 
 LiteRtStatus GpuOptions::EnableConstantTensorSharing(bool enabled) {
-  return LiteRtSetGpuOptionsConstantTensorSharing(Get(), enabled);
+  return LrtSetGpuOptionsConstantTensorsSharing(options_, enabled);
 }
 
 LiteRtStatus GpuOptions::EnableInfiniteFloatCapping(bool enabled) {
-  return LiteRtSetGpuOptionsInfiniteFloatCapping(Get(), enabled);
+  return LrtSetGpuOptionsInfiniteFloatCapping(options_, enabled);
 }
 
 LiteRtStatus GpuOptions::EnableBenchmarkMode(bool enabled) {
-  return LiteRtSetGpuOptionsBenchmarkMode(Get(), enabled);
+  return LrtSetGpuOptionsBenchmarkMode(options_, enabled);
 }
 
 Expected<void> GpuOptions::SetBackend(Backend backend) {
-  LITERT_RETURN_IF_ERROR(LiteRtSetGpuOptionsGpuBackend(
-      Get(), static_cast<LiteRtGpuBackend>(backend)));
+  LITERT_RETURN_IF_ERROR(LrtSetGpuOptionsGpuBackend(
+      options_, static_cast<LiteRtGpuBackend>(backend)));
   return {};
 }
 
 LiteRtStatus GpuOptions::EnableAllowSrcQuantizedFcConvOps(bool enabled) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsAllowSrcQuantizedFcConvOps(
-      Get(), enabled);
+  return LrtSetGpuAcceleratorCompilationOptionsAllowSrcQuantizedFcConvOps(
+      options_, enabled);
 }
 
 Expected<void> GpuOptions::SetPrecision(Precision precision) {
-  LITERT_RETURN_IF_ERROR(LiteRtSetGpuAcceleratorCompilationOptionsPrecision(
-      Get(), static_cast<LiteRtDelegatePrecision>(precision)));
+  LITERT_RETURN_IF_ERROR(LrtSetGpuAcceleratorCompilationOptionsPrecision(
+      options_, static_cast<LiteRtDelegatePrecision>(precision)));
   return {};
 }
 
 Expected<void> GpuOptions::SetBufferStorageType(BufferStorageType type) {
   LITERT_RETURN_IF_ERROR(
-      LiteRtSetGpuAcceleratorCompilationOptionsUseBufferStorageType(
-          Get(), static_cast<LiteRtDelegateBufferStorageType>(type)));
+      LrtSetGpuAcceleratorCompilationOptionsUseBufferStorageType(
+          options_, static_cast<LiteRtDelegateBufferStorageType>(type)));
   return {};
 }
 
 LiteRtStatus GpuOptions::SetPreferTextureWeights(bool prefer_texture_weights) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsPreferTextureWeights(
-      Get(), prefer_texture_weights);
+  return LrtSetGpuAcceleratorCompilationOptionsPreferTextureWeights(
+      options_, prefer_texture_weights);
 }
 
 LiteRtStatus GpuOptions::SetSerializationDir(const char* serialization_dir) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsSerializationDir(
-      Get(), serialization_dir);
+  return LrtSetGpuAcceleratorCompilationOptionsSerializationDir(
+      options_, serialization_dir);
 }
 
 LiteRtStatus GpuOptions::SetModelCacheKey(const char* model_cache_key) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsModelCacheKey(
-      Get(), model_cache_key);
+  return LrtSetGpuAcceleratorCompilationOptionsModelCacheKey(options_,
+                                                             model_cache_key);
 }
 
 LiteRtStatus GpuOptions::SetProgramCacheFd(int program_cache_fd) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsProgramCacheFd(
-      Get(), program_cache_fd);
+  return LrtSetGpuAcceleratorCompilationOptionsProgramCacheFd(options_,
+                                                              program_cache_fd);
 }
 
 LiteRtStatus GpuOptions::SetSerializeProgramCache(
     bool serialize_program_cache) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsSerializeProgramCache(
-      Get(), serialize_program_cache);
+  return LrtSetGpuAcceleratorCompilationOptionsSerializeProgramCache(
+      options_, serialize_program_cache);
 }
 
 LiteRtStatus GpuOptions::SetSerializeExternalTensors(
     bool serialize_external_tensors) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsSerializeExternalTensors(
-      Get(), serialize_external_tensors);
+  return LrtSetGpuAcceleratorCompilationOptionsSerializeExternalTensors(
+      options_, serialize_external_tensors);
 }
 
 LiteRtStatus GpuOptions::EnableExternalTensorsMode(bool enabled) {
-  return LiteRtSetGpuOptionsExternalTensorsMode(Get(), enabled);
+  return LrtSetGpuOptionsExternalTensorsMode(options_, enabled);
 }
 
 LiteRtStatus GpuOptions::AddExternalTensorPattern(const char* pattern) {
-  return LiteRtAddGpuOptionsExternalTensorPattern(Get(), pattern);
+  return LrtAddGpuOptionsExternalTensorPattern(options_, pattern);
 }
 
 LiteRtStatus GpuOptions::AddBufferStorageTensorPattern(const char* pattern) {
-  return LiteRtAddGpuOptionsBufferStorageTensorPattern(Get(), pattern);
+  return LrtAddGpuOptionsBufferStorageTensorPattern(options_, pattern);
 }
 
 Expected<void> GpuOptions::SetPriority(Priority priority) {
-  LITERT_RETURN_IF_ERROR(LiteRtSetGpuOptionsGpuPriority(
-      Get(), static_cast<LiteRtGpuPriority>(priority)));
+  LITERT_RETURN_IF_ERROR(LrtSetGpuOptionsGpuPriority(
+      options_, static_cast<LiteRtGpuPriority>(priority)));
   return {};
 }
 
 LiteRtStatus GpuOptions::SetMadviseOriginalSharedTensors(
     bool madvise_original_shared_tensors) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsMadviseOriginalSharedTensors(
-      Get(), madvise_original_shared_tensors);
+  return LrtSetGpuAcceleratorCompilationOptionsMadviseOriginalSharedTensors(
+      options_, madvise_original_shared_tensors);
 }
 
 LiteRtStatus GpuOptions::SetNumStepsOfCommandBufferPreparations(
     int num_steps_of_command_buffer_preparations) {
-  return
-      LiteRtSetGpuAcceleratorRuntimeOptionsNumStepsOfCommandBufferPreparations(
-          Get(), num_steps_of_command_buffer_preparations);
+  return LrtSetGpuAcceleratorRuntimeOptionsNumStepsOfCommandBufferPreparations(
+      options_, num_steps_of_command_buffer_preparations);
 }
 
 #ifdef __APPLE__
 LiteRtStatus GpuOptions::SetUseMetalArgumentBuffers(
     bool use_metal_argument_buffers) {
-  return LiteRtSetGpuOptionsUseMetalArgumentBuffers(Get(),
-                                                    use_metal_argument_buffers);
+  return LrtSetGpuOptionsUseMetalArgumentBuffers(options_,
+                                                 use_metal_argument_buffers);
 }
 #endif  // __APPLE__
 
 LiteRtStatus GpuOptions::SetSyncExecutionModeWaitType(
     SyncExecutionModeWaitType wait_type) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsWaitType(
-      Get(), static_cast<LiteRtGpuWaitType>(wait_type));
+  return LrtSetGpuAcceleratorRuntimeOptionsWaitType(
+      options_, static_cast<LiteRtGpuWaitType>(wait_type));
 }
 
 LiteRtStatus GpuOptions::SetPreferredDeviceSubstr(
     const char* preferred_device_substr) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsPreferredDeviceSubstr(
-      Get(), preferred_device_substr);
+  return LrtSetGpuAcceleratorRuntimeOptionsPreferredDeviceSubstr(
+      options_, preferred_device_substr);
 }
 
 LiteRtStatus GpuOptions::SetNumThreadsToUpload(int num_threads_to_upload) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsNumThreadsToUpload(
-      Get(), num_threads_to_upload);
+  return LrtSetGpuAcceleratorRuntimeOptionsNumThreadsToUpload(
+      options_, num_threads_to_upload);
 }
 
 LiteRtStatus GpuOptions::SetNumThreadsToCompile(int num_threads_to_compile) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsNumThreadsToCompile(
-      Get(), num_threads_to_compile);
+  return LrtSetGpuAcceleratorRuntimeOptionsNumThreadsToCompile(
+      options_, num_threads_to_compile);
 }
 
 LiteRtStatus GpuOptions::SetConvertWeightsOnGpu(bool convert_weights_on_gpu) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsConvertWeightsOnGpu(
-      Get(), convert_weights_on_gpu);
+  return LrtSetGpuAcceleratorRuntimeOptionsConvertWeightsOnGpu(
+      options_, convert_weights_on_gpu);
 }
 
 LiteRtStatus GpuOptions::SetHintFullyDelegatedToSingleDelegate(
     bool hint_fully_delegated_to_single_delegate) {
-  return LiteRtSetGpuOptionsHintFullyDelegatedToSingleDelegate(
-      Get(), hint_fully_delegated_to_single_delegate);
+  return LrtSetGpuOptionsHintFullyDelegatedToSingleDelegate(
+      options_, hint_fully_delegated_to_single_delegate);
 }
 
 LiteRtStatus GpuOptions::DisableShaderOptimization(bool disable) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsDisableShaderOptimization(
-      Get(), disable);
+  return LrtSetGpuAcceleratorCompilationOptionsDisableShaderOptimization(
+      options_, disable);
 }
 
 LiteRtStatus GpuOptions::WaitForWeightsConversionComplete(bool wait) {
-  return LiteRtSetGpuAcceleratorRuntimeOptionsWaitForWeightsConversionComplete(
-      Get(), wait);
+  return LrtSetGpuAcceleratorRuntimeOptionsWaitForWeightsConversionComplete(
+      options_, wait);
 }
 
 LiteRtStatus GpuOptions::CacheCompiledProgramsOnly(bool only) {
-  return LiteRtSetGpuAcceleratorCompilationOptionsCacheCompiledProgramsOnly(
-      Get(), only);
+  return LrtSetGpuAcceleratorCompilationOptionsCacheCompiledProgramsOnly(
+      options_, only);
 }
 
 }  // namespace litert
