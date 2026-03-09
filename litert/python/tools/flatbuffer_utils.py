5e4a2ae00cacb70d438b77d55b2044869e34892d
Pedro Gonnet <gonnet@google.com>
2026-03-09 07:38:56 -0700

    For large tensors, compute the quantization in chunks of the leading dimension to avoid generating large intermediate values.
    LiteRT-PiperOrigin-RevId: 880851055

--- litert/python/tools/flatbuffer_utils.py
+++ litert/python/tools/flatbuffer_utils.py
@@ -36,17 +36,22 @@ import os # import gfile
 from litert.python import schema_py_generated as schema_fb  # pylint:disable=g-direct-tensorflow-import
 
 # Types imported from `schema_py_generated`.
+ActivationFunctionType = schema_fb.ActivationFunctionType
+BlockwiseQuantizationT = schema_fb.BlockwiseQuantizationT
 Buffer = schema_fb.Buffer
 BufferT = schema_fb.BufferT
 BuiltinOperator = schema_fb.BuiltinOperator
 BuiltinOptions = schema_fb.BuiltinOptions
 BuiltinOptions2 = schema_fb.BuiltinOptions2
+FullyConnectedOptionsT = schema_fb.FullyConnectedOptionsT
 Model = schema_fb.Model
 ModelT = schema_fb.ModelT
 Operator = schema_fb.Operator
 OperatorCode = schema_fb.OperatorCode
 OperatorCodeT = schema_fb.OperatorCodeT
 OperatorT = schema_fb.OperatorT
+QuantizationDetails = schema_fb.QuantizationDetails
+QuantizationParametersT = schema_fb.QuantizationParametersT
 StableHLOCompositeOptions = schema_fb.StableHLOCompositeOptions
 StableHLOCompositeOptionsT = schema_fb.StableHLOCompositeOptionsT
 SubGraph = schema_fb.SubGraph
@@ -81,6 +86,65 @@ class Packable(Protocol):
 _TFLITE_FILE_IDENTIFIER = b'TFL3'
 
 _TENSOR_TYPE_TO_NAME = {v: k for k, v in TensorType.__dict__.items()}
+
+
+def update_packed_buffer(
+    packed_buffer: Buffer, offset: int | None = None, size: int | None = None
+):
+  """Sets the `offset` and `size` values in a packed `Buffer` object.
+
+  This is based on the implementation of `Buffer.Offset` and `Buffer.Size` in
+  `schema_py_generated.py`.
+
+  The `Bufffer` is declared as follows in the TFLite schema:
+  ```
+  table Buffer {
+    data:[ubyte] (force_align: 16);
+    offset: ulong;
+    size: ulong;
+  }
+  ```
+  i.e. the `offset` and `size` fields are of type `uint64` and reside at offsets
+  6 and 8 of the `Buffer` vtable, respectively.
+
+  Note that for this to work, the original `Buffer` object has to have had
+  non-default values for `offset` and `size` set.
+
+  Args:
+    packed_buffer: The packed `Buffer` object to modify.
+    offset: Integer offset value.
+    size: Integer size value.
+
+  Raises:
+    `ValueError` if the original `Buffer` object has no spece reserved for
+    either `offset` or `size`.
+  """
+  table = packed_buffer._tab  # pylint: disable=protected-access
+  packer_type = flatbuffers.number_types.Uint64Flags.packer_type
+  if offset is not None:
+    vtable_offset = flatbuffers.number_types.UOffsetTFlags.py_type(
+        table.Offset(6)
+    )
+    if vtable_offset == 0:
+      raise ValueError('Failed to set `offset`, no buffer reserved for it.')
+    flatbuffers.encode.Write(
+        packer_type,
+        table.Bytes,
+        vtable_offset + table.Pos,
+        offset,
+    )
+  if size is not None:
+    vtable_offset = flatbuffers.number_types.UOffsetTFlags.py_type(
+        table.Offset(8)
+    )
+    if vtable_offset == 0:
+      raise ValueError('Failed to set `size`, no buffer reserved for it.')
+    flatbuffers.encode.Write(
+        packer_type,
+        table.Bytes,
+        vtable_offset + table.Pos,
+        size,
+    )
 
 
 def get_builtin_code_from_operator_code(
@@ -271,8 +335,9 @@ def convert_object_to_bytearray(
   builder = flatbuffers.Builder(1024)
   model_offset = model_object.Pack(builder)
   builder.Finish(model_offset, file_identifier=_TFLITE_FILE_IDENTIFIER)
-  model_bytearray = bytearray(builder.Output())
-  model_bytearray = model_bytearray + extra_buffer
+  model_bytearray = builder.Output()
+  if extra_buffer:
+    model_bytearray = model_bytearray + extra_buffer
   return model_bytearray
 
 
