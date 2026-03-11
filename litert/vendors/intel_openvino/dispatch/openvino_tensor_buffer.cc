8768008ef085fd65e8a3063c9eeb6c0a870a514b
Zhaoliang Ma <zhaoliang.ma@intel.com>
2026-03-11 16:02:14 +0800

    Refactor RemoteTensorBuffer to generic OpenVino tensor
    This change reduces direct dependency on Level Zero internal calls
    and leverages OpenVINO's built-in memory management. By using the
    recommended generic API, code readability and maintainability are
    improved within the delegate while keeping the functional
    behavior unchanged.
    
    Test: .\run_model.exe --accelerator=npu --intel_openvino_device_type=npu
    --compiler_plugin_library_dir=... --dispatch_library_dir=... --graph=...

--- litert/vendors/intel_openvino/dispatch/openvino_tensor_buffer.cc
+++ litert/vendors/intel_openvino/dispatch/openvino_tensor_buffer.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "litert/vendors/intel_openvino/dispatch/remote_tensor_buffer.h"
+#include "litert/vendors/intel_openvino/dispatch/openvino_tensor_buffer.h"
 
 #include <cstddef>
 #include <cstdint>
@@ -29,43 +29,40 @@
 #include "litert/vendors/intel_openvino/dispatch/openvino_shared_core.h"
 #include "litert/vendors/intel_openvino/utils.h"
 
-litert::Expected<void> RemoteTensorBuffer::Alloc(
+litert::Expected<void> OpenVinoTensorBuffer::Alloc(
     const LiteRtRankedTensorType& tensor_type, size_t size) {
   if (allocated_) {
     return litert::Unexpected(kLiteRtStatusErrorInvalidArgument,
-                              "The remote tensor has been allocated.");
+                              "The OpenVino tensor has been allocated.");
   }
+
   // TODO:: Release the shared OpenVINO Core.
   std::shared_ptr<ov::Core> core = OpenVINOSharedCore::GetInstance()->getCore();
-  auto context = core->get_default_context("NPU")
-                     .as<ov::intel_npu::level_zero::ZeroContext>();
+  auto context = core->get_default_context("NPU");
   ov::element::Type ov_element_type =
       litert::openvino::MapLiteTypeToOV(tensor_type.element_type);
   std::vector<int32_t> ov_shape_vec(tensor_type.layout.rank);
   for (size_t i = 0; i < ov_shape_vec.size(); i++)
     ov_shape_vec[i] = tensor_type.layout.dimensions[i];
-  auto level_zero_buffer = context.create_l0_host_tensor(
-      ov_element_type, ov::Shape{ov_shape_vec.begin(), ov_shape_vec.end()});
-
-  level_zero_buffer_ = level_zero_buffer;
+  host_tensor_ = context.create_host_tensor(
+       ov_element_type, ov::Shape{ov_shape_vec.begin(), ov_shape_vec.end()});
   allocated_ = true;
 
   return {};
 }
 
-litert::Expected<void*> RemoteTensorBuffer::GetZeroBufferPtr() {
+litert::Expected<void*> OpenVinoTensorBuffer::GetTensorData() {
   if (!allocated_) {
     return litert::Unexpected(kLiteRtStatusErrorInvalidArgument,
                               "The remote tensor didn't allocate.");
   }
-  return level_zero_buffer_.get();
+  return host_tensor_.data();
 }
 
-litert::Expected<ov::intel_npu::level_zero::ZeroBufferTensor>
-RemoteTensorBuffer::GetZeroBufferTensor() {
+litert::Expected<ov::Tensor> OpenVinoTensorBuffer::GetOVTensor() {
   if (!allocated_) {
     return litert::Unexpected(kLiteRtStatusErrorInvalidArgument,
                               "Failed to get zero buffer remote tensor.");
   }
-  return level_zero_buffer_;
+  return host_tensor_;
 }
---
+++ litert/vendors/intel_openvino/dispatch/openvino_tensor_buffer.h
@@ -0,0 +1,46 @@
+// Copyright 2025 Google LLC.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ODML_LITERT_LITERT_VENDORS_OPENVINO_DISPATCH_OPENVINO_TENSOR_BUFFER_H_
+#define ODML_LITERT_LITERT_VENDORS_OPENVINO_DISPATCH_OPENVINO_TENSOR_BUFFER_H_
+
+#include <cstddef>
+
+#include "openvino/runtime/tensor.hpp"
+#include "litert/c/litert_model_types.h"
+#include "litert/cc/litert_expected.h"
+
+class OpenVinoTensorBuffer {
+ public:
+  OpenVinoTensorBuffer(const OpenVinoTensorBuffer&) = delete;
+  OpenVinoTensorBuffer& operator=(const OpenVinoTensorBuffer&) = delete;
+  OpenVinoTensorBuffer(OpenVinoTensorBuffer&&) = default;
+  OpenVinoTensorBuffer& operator=(OpenVinoTensorBuffer&&) = default;
+
+  OpenVinoTensorBuffer() : host_tensor_({}), allocated_(false) {};
+  ~OpenVinoTensorBuffer() = default;
+
+  litert::Expected<void> Alloc(const LiteRtRankedTensorType& tensor_type,
+                               size_t size);
+
+  litert::Expected<void*> GetTensorData();
+
+  litert::Expected<ov::Tensor> GetOVTensor();
+
+ private:
+  ov::Tensor host_tensor_;
+  bool allocated_;
+};
+
+#endif  // ODML_LITERT_LITERT_VENDORS_OPENVINO_DISPATCH_OPENVINO_TENSOR_BUFFER_H_
