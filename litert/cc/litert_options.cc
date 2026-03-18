d7c4b2021de1e1ef7a8ac8aaae2f5ddb77ecf364
jiseong oh <jiseong.oh@samsung.com>
2026-03-18 14:10:08 +0900

    Add samsung option for run model
    - Add Initial option file
    - Fixed compilation issue due to be updated a function of dispatch_api.
    
    Signed-off-by: jiseong oh <jiseong.oh@samsung.com>

--- litert/cc/litert_options.cc
+++ litert/cc/litert_options.cc
@@ -40,6 +40,7 @@
 #include "litert/cc/options/litert_intel_openvino_options.h"
 #include "litert/cc/options/litert_mediatek_options.h"
 #include "litert/cc/options/litert_qualcomm_options.h"
+#include "litert/cc/options/litert_samsung_options.h"
 #include "litert/cc/options/litert_runtime_options.h"
 #if defined(LITERT_WITH_EXTERNAL_WEIGHT_LOADER)
 #include "litert/core/options.h"
@@ -117,6 +118,10 @@ Options::GetIntelOpenVinoOptions() {
   return EnsureOption(intel_openvino_options_);
 }
 
+Expected<samsung::SamsungOptions&> Options::GetSamsungOptions() {
+  return EnsureOption(samsung_options_);
+}
+
 Expected<RuntimeOptions&> Options::GetRuntimeOptions() {
   return EnsureOption(runtime_options_);
 }
@@ -138,6 +143,7 @@ Expected<void> Options::Build() {
       Get(), google_tensor_options_, LrtGetOpaqueGoogleTensorOptionsData));
   LITERT_RETURN_IF_ERROR(AppendAndResetOpaqueData(
       Get(), intel_openvino_options_, LrtGetOpaqueIntelOpenVinoOptionsData));
+  LITERT_RETURN_IF_ERROR(AppendAndReset(Get(), samsung_options_));
   LITERT_RETURN_IF_ERROR(AppendAndResetOpaqueData(
       Get(), runtime_options_, LrtGetOpaqueRuntimeOptionsData));
   LITERT_RETURN_IF_ERROR(AppendAndResetOpaqueData(
