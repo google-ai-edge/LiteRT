49dfe320fcaf994de8a84355119e652d23a78ed1
Google AI Edge <ai-edge-bot@google.com>
2026-03-19 21:37:50 -0700

    Automated Code Change
    PiperOrigin-RevId: 886548566

--- tflite/core/async/c/async_signature_runner.cc
+++ tflite/core/async/c/async_signature_runner.cc
@@ -14,6 +14,9 @@ limitations under the License.
 ==============================================================================*/
 #include "tflite/core/async/c/async_signature_runner.h"
 
+#include <cstddef>
+#include <cstdint>
+
 #include "tflite/c/c_api_internal.h"
 #include "tflite/core/async/async_signature_runner.h"
 #include "tflite/core/async/c/internal.h"
