da8efbd9faca45d1c364a4903c090706e53cc9c4
jiseong oh <jiseong.oh@samsung.com>
2026-03-23 18:49:55 +0900

    Support Cast, Concat and EltDiv
    Changes to add support for Cast, Concat, EltDiv, Exp, Logistic and
    Softmax
    
    Co-authored-by Mashal.rane <mashal.rane@samsung.com>
    Signed-off-by: jiseong oh <jiseong.oh@samsung.com>

---
+++ litert/vendors/samsung/compiler/builders/cast_op_builder.cc
@@ -0,0 +1,51 @@
+// Copyright (C) 2026 Samsung Electronics Co. LTD.
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "litert/vendors/samsung/compiler/builders/cast_op_builder.h"
+
+#include "litert/c/litert_common.h"
+#include "litert/c/litert_op_options.h"
+#include "litert/cc/litert_expected.h"
+#include "litert/cc/litert_model.h"
+#include "litert/vendors/samsung/compiler/builders/utils.h"
+
+namespace litert::samsung {
+
+Expected<OpWrapper> BuildCastOp(const Op &op) {
+  OpWrapper op_wrapper("Cast");
+
+  for (const auto &input : op.Inputs()) {
+    op_wrapper.AddInput(input);
+  }
+  for (const auto &output : op.Outputs()) {
+    op_wrapper.AddOutput(output);
+
+    auto ranked_tensor_type = output.RankedTensorType();
+    if (!ranked_tensor_type) {
+      return ranked_tensor_type.Error();
+    }
+    auto element_type = ranked_tensor_type->ElementType();
+
+    auto element_type_mapping = ConvertElementTypeToInt(element_type);
+    if (!element_type_mapping.HasValue()) {
+      return element_type_mapping.Error();
+    }
+
+    op_wrapper.AddParam("to", element_type_mapping.Value());
+  }
+
+  return op_wrapper;
+}
+}  // namespace litert::samsung
--- litert/vendors/samsung/compiler/builders/utils.cc
+++ litert/vendors/samsung/compiler/builders/utils.cc
@@ -18,7 +18,38 @@
 
 namespace litert::samsung {
 
+enum DATA_TYPE {
+    UNDEFINED = 0,
+    FLOAT32,
+    UINT8,
+    INT8,
+    UINT16,
+    INT16,
+    INT32,
+    INT64,
+    STRING,
+    BOOL,
+    FLOAT16,
+    DOUBLE,
+    UINT32,
+    UINT64,
+    COMPLEX64,
+    COMPLEX128,
+    BFLOAT16,
+    FLOAT8E4M3FN,
+    FLOAT8E4M3FNUZ,
+    FLOAT8E5M2,
+    FLOAT8E5M2FNUZ,
+    UINT4,
+    INT4,
+    FLOAT4E2M1,
+    FLOAT8E8M0,
+    UINT2,
+    INT2
+};
+
 Expected<std::string> GetFusedActivationName(uint32_t tfl_fused_activation) {
+
   if (tfl_fused_activation > tflite::ActivationFunctionType_MAX) {
     return Error(kLiteRtStatusErrorIndexOOB, "Invalid activation");
   }
@@ -40,6 +71,34 @@ GetDimensions(const Tensor &t) {
   //  Have no idea why get incorrect dimensions when use Span.
   return absl::InlinedVector<int32_t, kExpectedMaxTensorRank>(
       dimensions.begin(), dimensions.end());
+}
+
+Expected<const uint32_t> ConvertElementTypeToInt(ElementType element_type) {
+  switch (element_type) {
+  case ElementType::Bool:
+    return DATA_TYPE::BOOL;
+  case ElementType::Int4:
+    return DATA_TYPE::INT4;
+  case ElementType::Int8:
+    return DATA_TYPE::INT8;
+  case ElementType::UInt8:
+    return DATA_TYPE::UINT8;
+  case ElementType::Int16:
+    return DATA_TYPE::UINT16;
+  case ElementType::UInt16:
+    return DATA_TYPE::UINT16;
+  case ElementType::Int32:
+    return DATA_TYPE::INT32;
+  case ElementType::Int64:
+    return DATA_TYPE::INT64;
+  case ElementType::Float16:
+    return DATA_TYPE::FLOAT16;
+  case ElementType::Float32:
+    return DATA_TYPE::FLOAT32;
+  default:
+    return Error(kLiteRtStatusErrorRuntimeFailure,
+                 "Element Type not supported");
+  }
 }
 
 } // namespace litert::samsung
