e6802ab7895b5e95e1693a59ca0c1f743a0d35af
Pedro Gonnet <gonnet@google.com>
2026-04-15 05:52:04 -0700

    Update dependencies for `xnnpack`, `pthreadpool`, and `slinky`.
    PiperOrigin-RevId: 900118966

--- tflite/tools/cmake/modules/xnnpack.cmake
+++ tflite/tools/cmake/modules/xnnpack.cmake
@@ -23,7 +23,7 @@ OverridableFetchContent_Declare(
   xnnpack
   GIT_REPOSITORY https://github.com/google/XNNPACK
   # Sync with tensorflow/workspace2.bzl
-  GIT_TAG 132a7b041d8f8ef74172d3a8cd509d6751fdf33d
+  GIT_TAG bf362487d3c00ed7040a0c9c4b885f2ba20a6c45
   GIT_PROGRESS TRUE
   PREFIX "${CMAKE_BINARY_DIR}"
   SOURCE_DIR "${CMAKE_BINARY_DIR}/xnnpack"
