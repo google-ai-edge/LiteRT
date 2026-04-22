a5ca9ad67e8c3f995f3342a2b3cd2bcf699f6a1c
Andrew Zhang <yunandrew@google.com>
2026-04-22 14:44:04 -0700

    Cleanup corrupted model cache that failed to be loaded.
    LiteRT-PiperOrigin-RevId: 904054258

--- litert/c/litert_environment_options.h
+++ litert/c/litert_environment_options.h
@@ -62,7 +62,8 @@ typedef enum {
   kLiteRtEnvOptionTagAutoRegisterAccelerators = 24,
   // Minimum logger severity for the environment.
   kLiteRtEnvOptionTagMinLoggerSeverity = 25,
-
+  kLiteRtEnvOptionTagCompilerCacheMaxConfigsPerModel = 26,
+  kLiteRtEnvOptionTagCompilerCacheMaxTotalSize = 27,
   // Internal use only. Virtual null tag for option that is not defined.
   kLiteRtEnvOptionTagNull = 255,
 } LiteRtEnvOptionTag;
