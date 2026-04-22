15efb0d9461ccfd974542e34a83785343330db4a
Andrew Zhang <yunandrew@google.com>
2026-04-22 00:43:28 -0700

    Add get sdk version api in compiler plugin.
    LiteRT-PiperOrigin-RevId: 903665995

--- litert/vendors/qualcomm/compiler/qnn_compiler_plugin.cc
+++ litert/vendors/qualcomm/compiler/qnn_compiler_plugin.cc
@@ -130,6 +130,16 @@ LiteRtStatus LiteRtGetCompilerPluginSupportedSocModel(
   return kLiteRtStatusOk;
 }
 
+LiteRtStatus LiteRtGetCompilerPluginSDKVersion(
+    LiteRtCompilerPlugin compiler_plugin, const char** sdk_version) {
+  if (!compiler_plugin || !sdk_version) {
+    return kLiteRtStatusErrorInvalidArgument;
+  }
+  // No-op implementation for Qualcomm plugin.
+  *sdk_version = "";
+  return kLiteRtStatusOk;
+}
+
 //
 // Compiled Result Definition
 //
