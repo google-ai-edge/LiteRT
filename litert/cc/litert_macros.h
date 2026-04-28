4452887cdedaf5e089a7d2a0efbc6a0c2ea773e2
Google AI Edge <ai-edge-bot@google.com>
2026-04-28 11:02:14 -0700

    Inline litert macros implementation into header files
    LiteRT-PiperOrigin-RevId: 907072757

--- litert/cc/litert_macros.h
+++ litert/cc/litert_macros.h
@@ -472,4 +472,149 @@ class LogBeforeAbort {
     *e = init;                                  \
   }
 
+namespace litert::internal::macros_detail {
+
+inline LiteRtStatus ToLiteRtStatus(const absl::StatusCode& code) {
+  switch (code) {
+    case absl::StatusCode::kOk:
+      return kLiteRtStatusOk;
+    case absl::StatusCode::kCancelled:
+      return kLiteRtStatusErrorTimeoutExpired;
+    case absl::StatusCode::kUnknown:
+      return kLiteRtStatusErrorUnknown;
+    case absl::StatusCode::kInvalidArgument:
+      return kLiteRtStatusErrorInvalidArgument;
+    case absl::StatusCode::kDeadlineExceeded:
+      return kLiteRtStatusErrorTimeoutExpired;
+    case absl::StatusCode::kNotFound:
+      return kLiteRtStatusErrorNotFound;
+    case absl::StatusCode::kAlreadyExists:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kPermissionDenied:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kResourceExhausted:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kFailedPrecondition:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kAborted:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kOutOfRange:
+      return kLiteRtStatusErrorIndexOOB;
+    case absl::StatusCode::kUnimplemented:
+      return kLiteRtStatusErrorUnsupported;
+    case absl::StatusCode::kInternal:
+      return kLiteRtStatusErrorUnknown;
+    case absl::StatusCode::kUnavailable:
+      return kLiteRtStatusErrorNotFound;
+    case absl::StatusCode::kDataLoss:
+      return kLiteRtStatusErrorRuntimeFailure;
+    case absl::StatusCode::kUnauthenticated:
+      return kLiteRtStatusErrorRuntimeFailure;
+    default:
+      return kLiteRtStatusErrorUnknown;
+  }
+  return kLiteRtStatusErrorUnknown;
+}
+
+}  // namespace litert::internal::macros_detail
+
+namespace litert {
+
+inline litert::Error ErrorStatusBuilder::ErrorConversion<absl::Status>::AsError(
+    const absl::Status& value) {
+  return litert::Error(internal::macros_detail::ToLiteRtStatus(value.code()),
+                       std::string(value.message()));
+}
+
+inline absl::Status ErrorStatusBuilder::ToAbslStatus() const noexcept {
+  switch (error_.StatusCC()) {
+    case Status::kOk:
+      return absl::OkStatus();
+    case Status::kErrorInvalidArgument:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kErrorMemoryAllocationFailure:
+      return absl::ResourceExhaustedError(LogMessage());
+    case Status::kErrorRuntimeFailure:
+      return absl::InternalError(LogMessage());
+    case Status::kErrorMissingInputTensor:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kErrorUnsupported:
+      return absl::UnimplementedError(LogMessage());
+    case Status::kErrorNotFound:
+      return absl::NotFoundError(LogMessage());
+    case Status::kErrorTimeoutExpired:
+      return absl::DeadlineExceededError(LogMessage());
+    case Status::kCancelled:
+      return absl::CancelledError(LogMessage());
+    case Status::kErrorWrongVersion:
+      return absl::FailedPreconditionError(LogMessage());
+    case Status::kErrorUnknown:
+      return absl::UnknownError(LogMessage());
+    case Status::kErrorAlreadyExists:
+      return absl::AlreadyExistsError(LogMessage());
+    case Status::kErrorFileIO:
+      return absl::UnavailableError(LogMessage());
+    case Status::kErrorInvalidFlatbuffer:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kErrorDynamicLoading:
+      return absl::UnavailableError(LogMessage());
+    case Status::kErrorSerialization:
+      return absl::InternalError(LogMessage());
+    case Status::kErrorCompilation:
+      return absl::InternalError(LogMessage());
+    case Status::kErrorIndexOOB:
+      return absl::OutOfRangeError(LogMessage());
+    case Status::kErrorInvalidIrType:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kErrorInvalidGraphInvariant:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kErrorGraphModification:
+      return absl::InternalError(LogMessage());
+    case Status::kErrorInvalidToolConfig:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kLegalizeNoMatch:
+      return absl::NotFoundError(LogMessage());
+    case Status::kErrorInvalidLegalization:
+      return absl::InvalidArgumentError(LogMessage());
+    case Status::kPatternNoMatch:
+      return absl::NotFoundError(error_.Message());
+    case Status::kInvalidTransformation:
+      return absl::InvalidArgumentError(error_.Message());
+    case Status::kErrorUnsupportedRuntimeVersion:
+      return absl::FailedPreconditionError(LogMessage());
+    case Status::kErrorUnsupportedCompilerVersion:
+      return absl::FailedPreconditionError(LogMessage());
+    case Status::kErrorIncompatibleByteCodeVersion:
+      return absl::FailedPreconditionError(LogMessage());
+    case Status::kErrorShapeInferenceUnsupportedOp:
+      return absl::UnimplementedError(LogMessage());
+    case Status::kErrorShapeInferenceFailed:
+      return absl::InvalidArgumentError(LogMessage());
+  }
+  return absl::UnknownError(LogMessage());
+}
+
+inline std::string ErrorStatusBuilder::LogMessage() const {
+  LiteRtLogger logger = LiteRtGetDefaultLogger();
+  LiteRtLogSeverity min_severity;
+  if (LiteRtGetMinLoggerSeverity(logger, &min_severity) != kLiteRtStatusOk) {
+    min_severity = kLiteRtLogSeverityVerbose;
+  }
+  if (log_level_ >= min_severity) {
+    std::stringstream sstr;
+    sstr << LiteRtGetLogSeverityName(log_level_) << ": [" << loc_.file_name()
+         << ':' << loc_.line() << ']';
+    if (extra_log_) {
+      sstr << ' ' << extra_log_->str();
+    }
+    if (!error_.Message().empty()) {
+      sstr << "\n└ " << error_.Message();
+    }
+    return sstr.str();
+  }
+  return "";
+}
+
+}  // namespace litert
+
 #endif  // ODML_LITERT_LITERT_CC_LITERT_MACROS_H_
