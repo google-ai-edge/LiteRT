352642378628f54b9a0b0c3e82ec59d7e2dc2271
Google AI Edge <ai-edge-bot@google.com>
2026-05-18 21:10:29 -0700

    Initial Implementation of Streamed Loading to WebGPU on LiteRT.js (5/6)
    LiteRT-PiperOrigin-RevId: 917581861

--- weight_loader/external_weight_loader_litert.h
+++ weight_loader/external_weight_loader_litert.h
@@ -20,9 +20,18 @@
 #include <optional>
 #include <string>
 
+#include "absl/container/flat_hash_map.h"  // from @com_google_absl
 #include "absl/status/status.h"  // from @com_google_absl
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "absl/types/span.h"  // from @com_google_absl
+#ifdef __EMSCRIPTEN__
+// copybara:comment_begin
+#include <webgpu/webgpu_cpp.h>
+// copybara:comment_end
+// copybara:uncomment_begin
+// #include "ml_drift/webgpu/webgpu_headers.h"  // from @ml_drift
+// copybara:uncomment_end
+#endif  // __EMSCRIPTEN__
 #include "litert/c/internal/litert_runtime_context.h"
 #include "litert/c/litert_common.h"
 #include "litert/cc/internal/scoped_weight_source.h"
@@ -132,6 +141,14 @@ class WeightLoader {
   // buffer ID. Returns `nullptr` if no such tensor exists.
   virtual const WeightAccess* GetExternalWeightByBuffer(
       uint32_t external_buffer_id) const = 0;
+
+#ifdef __EMSCRIPTEN__
+  virtual absl::Status UploadWeightsOnWeb(
+      const absl::flat_hash_map<int, wgpu::Buffer>& tfl_id_to_wgpu_buffer) {
+    return absl::UnimplementedError(
+        "UploadWeightsOnWeb is not implemented by default.");
+  }
+#endif  // __EMSCRIPTEN__
 
   // Marks the host mapping for the external weight tensor as discardable.
   virtual absl::Status DiscardExternalWeightByBuffer(
