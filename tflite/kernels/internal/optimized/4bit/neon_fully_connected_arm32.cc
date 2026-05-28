4fadbd7c49e1d01b1f24d6da7b2bb5501e05d70b
Robert David <lrdx@google.com>
2026-05-27 19:16:22 -0700

    Use input/output operand for loop counter in 4-bit fully connected kernels.
    The `run_depth` variable is now passed as an input/output operand to the inline assembly, allowing the compiler to choose the register for the loop counter. This removes the hardcoded use of `w3` or `r3`. The `run_depth` is also re-initialized inside the inner loop for each block.
    
    PiperOrigin-RevId: 922475185

--- tflite/kernels/internal/optimized/4bit/neon_fully_connected_arm32.cc
+++ tflite/kernels/internal/optimized/4bit/neon_fully_connected_arm32.cc
@@ -52,7 +52,6 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
   const int outer_rows = (clamped_end_row + rows_left - 1) / rows_left;
   const int outer_cols = (clamped_end_col + rows_right - 1) / rows_right;
   const int depth = std::min(lhs_layout_cols / cols, rhs_layout_cols / cols);
-  const int run_depth = depth;
   for (int i = start_row; i < outer_rows; ++i) {
     const int left_index = i * rows_left * lhs_layout_cols / 2;
     const uint8_t* lhs_ptr_data = lhs + left_index;
@@ -60,6 +59,7 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
       const uint8_t* lhs_ptr = lhs_ptr_data;
       const int right_index = j * rows_right * rhs_layout_cols;
       const int8_t* rhs_ptr = rhs + right_index;
+      int run_depth = depth;
       asm volatile(
           R"asm(
           vmov.i8 q14, #15
@@ -81,8 +81,7 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
           vld1.8 {d26, d27}, [%[rhs_ptr]]!
           vshr.u8 q6, q6, #4
           vshr.u8 q7, q7, #4
-          mov r3, %[run_depth]
-          subs r3, r3, #1
+          subs %[run_depth], %[run_depth], #1
           bls 1f /* skip loop */
             0: /* loop start */
             vmlal.s8 q0, d8, d24
@@ -112,7 +111,7 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
             vshr.u8 q5, q5, #4
             vshr.u8 q6, q6, #4
             vshr.u8 q7, q7, #4
-            subs r3, r3, #1
+            subs %[run_depth], %[run_depth], #1
             bhi 0b /* loop branch */
           1: /* loop end */
           vmlal.s8 q0, d8, d24
@@ -143,10 +142,11 @@ void NeonRunKernelNoSDot<4, 1, 32>(const uint8_t* lhs, const int8_t* rhs,
           vpadd.i32 d5, d2, d3
           vst1.32 {d4, d5}, [%[dst]]!
           )asm"
-          : [lhs_ptr] "+r"(lhs_ptr), [rhs_ptr] "+r"(rhs_ptr), [dst] "+r"(dst)
-          : [run_depth] "r"(run_depth)
-          : "cc", "memory", "r3", "d0", "d1", "d2", "d3", "d4", "d5", "d8",
-            "d9", "d10", "d11", "d12", "d13", "d14", "d15", "d16", "d17", "d18",
+          : [lhs_ptr] "+r"(lhs_ptr), [rhs_ptr] "+r"(rhs_ptr), [dst] "+r"(dst),
+            [run_depth] "+r"(run_depth)
+          :
+          : "cc", "memory", "d0", "d1", "d2", "d3", "d4", "d5", "d8", "d9",
+            "d10", "d11", "d12", "d13", "d14", "d15", "d16", "d17", "d18",
             "d19", "d20", "d21", "d22", "d23", "d24", "d25", "d26", "d27", "q0",
             "q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "q9", "q10", "q11",
             "q14");
