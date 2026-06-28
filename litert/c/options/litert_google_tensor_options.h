b0dd80f1aa1cac6bac3872016d9fa36d2fb91c2b
Google AI Edge <ai-edge-bot@google.com>
2026-06-27 23:06:12 -0700

    Internal change
    LiteRT-PiperOrigin-RevId: 939269176

--- litert/c/options/litert_google_tensor_options.h
+++ litert/c/options/litert_google_tensor_options.h
@@ -139,6 +139,16 @@ LiteRtStatus LrtGoogleTensorOptionsSetExtraOptionsPath(
 LiteRtStatus LrtGoogleTensorOptionsGetExtraOptionsPath(
     LrtGoogleTensorOptions options, const char** extra_options_path);
 
+// copybara:uncomment_begin(google-only)
+// // experimental_enable_input_validator ---------------------------------
+// 
+// LiteRtStatus LrtGoogleTensorOptionsSetExperimentalEnableInputValidator(
+//     LrtGoogleTensorOptions options, bool experimental_enable_input_validator);
+// 
+// LiteRtStatus LrtGoogleTensorOptionsGetExperimentalEnableInputValidator(
+//     LrtGoogleTensorOptions options, bool* experimental_enable_input_validator);
+// copybara:uncomment_end
+
 #ifdef __cplusplus
 }  // extern "C"
 #endif  // __cplusplus
