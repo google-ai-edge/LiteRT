b043614742100b8ae613d75a6019fd90e472271a
Byungchul Kim <byungchul@google.com>
2026-07-28 05:57:46 -0700

    Add more detailed comments for SetSharedTensorMaps GPU option
    - Add note that it's an internal usage only and requires client should be built with the same toolchain of LiteRT
    
    LiteRT-PiperOrigin-RevId: 955210996

--- litert/cc/options/litert_gpu_options.h
+++ litert/cc/options/litert_gpu_options.h
@@ -201,7 +201,20 @@ class GpuOptions {
     return LrtAddGpuOptionsBufferStorageTensorPattern(options_, pattern);
   }
 
+  /// @internal
+  /// WARNING: This API is not ABI-stable and only for internal usage. It works
+  /// only when the client is built together with the LiteRT and ML Drift
+  /// delegate. Unless you know what you are doing, do not use this API.
+  ///
   /// @brief Sets the pointer to the shared tensor maps.
+  /// It should be used only to share the tensor maps among the GPU delegates of
+  /// separate models.
+  ///
+  /// @note The caller is responsible for maintaining the lifetime of the struct
+  /// until the model is destroyed.
+  ///
+  /// @param shared_tensor_maps pointer of litert::ml_drift::SharedTensorMaps
+  /// struct.
   LiteRtStatus SetSharedTensorMaps(void* shared_tensor_maps) {
     return LrtSetGpuAcceleratorCompilationOptionsSharedTensorMaps(
         options_, shared_tensor_maps);
