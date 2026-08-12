af9ec84b0b2ada76607f8735a6b9dc7200365e98
Reilly Grant <reillyg@google.com>
2026-08-12 16:40:42 -0700

    Replace direct ruy profiler includes with compatibility.h in TFLite kernels
    Include `third_party/ruy/ruy/profiler/instrumentation.h` from
    `third_party/tensorflow/lite/kernels/internal/compatibility.h` and replace direct
    includes of `instrumentation.h` across TFLite internal optimized and reference
    kernels with `compatibility.h`.
    
    A follow-up change will make this include conditional on TFLITE_WITH_RUY.
    
    PiperOrigin-RevId: 963728821

--- tflite/kernels/internal/reference/sub.h
+++ tflite/kernels/internal/reference/sub.h
@@ -19,7 +19,6 @@ limitations under the License.
 
 #include <algorithm>
 
-#include "ruy/profiler/instrumentation.h"  // from @ruy
 #include "tflite/kernels/internal/common.h"
 #include "tflite/kernels/internal/compatibility.h"
 #include "tflite/kernels/internal/reference/broadcast_loop.h"
