466bb561ad8b2ee3b28deccaa31d5b86b3aad5a8
Ming-Hsiu Pan <mingxiup@qti.qualcomm.com>
2026-08-21 11:47:09 +0800

    Qualcomm AI Engine Direct - Support ExpandDims Op. Summary: - Add ExpandDims Op Builder - Add corresponding unit test

--- litert/vendors/qualcomm/compiler/qnn_compose_graph.cc
+++ litert/vendors/qualcomm/compiler/qnn_compose_graph.cc
@@ -1608,6 +1608,7 @@ GetOpBuilders() {
       Adapt<BuildDynamicUpdateSliceOp>;
   builders[kLiteRtOpCodeTflRelu0To1] = Adapt<BuildRelu0To1Op>;
   builders[kLiteRtOpCodeTflSign] = Adapt<BuildSignOp>;
+  builders[kLiteRtOpCodeTflExpandDims] = Adapt<BuildReshapeOp>;
   builders[kLiteRtOpCodeShloComposite] = Adapt<BuildShloCompositeOp>;
   return builders;
 }
