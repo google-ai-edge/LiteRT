79ff0f3ebec0dfb32bd72c508018ca05f5f3136d
destro4evr <destro4evr@gmail.com>
2026-09-01 08:17:31 -0700

    Copybara import of the project:
    --
    f46c3add580e2b5b5214f77dc62c717098a7f94c by destro4evr-rgb <destro4evr@proton.me>:
    
    litert/xnnpack: reject negative tensor dimensions in DefineValue()
    
    A crafted TFLite model with a negative dimension value (e.g. -1) in a
    channelwise-quantized tensor's shape causes DefineValue() to silently
    cast the int32_t -1 to size_t (~1.8e19), which is then passed as the
    dimension count to xnn_validate_channelwise_quantized_tensor(). That
    function loops over dims[channel_dim] scale entries, reading far past
    the end of the scale array and crashing with SIGSEGV at model-load time.
    
    Reject negative dimensions explicitly instead of relying on downstream
    code to detect the implausible size_t value.
    
    --
    ffcc2dcb5d3930a485fcf47d58247110182cb52f by destro4evr-rgb <destro4evr@proton.me>:
    
    address review: use absl::c_any_of and keep range constructor
    
    --
    049a93a1f0b5da383e06984a30f3e91f1709f562 by destro4evr-rgb <destro4evr@proton.me>:
    
    conversion: add missing absl/algorithm:container and absl/strings:str_format deps
    
    absl::c_any_of requires absl/algorithm/container.h and
    absl::StrFormat requires absl/strings/str_format.h; both were
    missing from the include list and the BUILD deps for the
    conversion cc_library target.
    FUTURE_COPYBARA_INTEGRATE_REVIEW=https://github.com/google/XNNPACK/pull/11092 from destro4evr-rgb:fix/litert-xnnpack-negative-dim-oob-read 049a93a1f0b5da383e06984a30f3e91f1709f562
    LiteRT-PiperOrigin-RevId: 974506836

--- tensor/backends/xnnpack/conversion.cc
+++ tensor/backends/xnnpack/conversion.cc
@@ -27,6 +27,7 @@ limitations under the License.
 #include <vector>
 
 #include "xnnpack.h"  // from @XNNPACK
+#include "absl/algorithm/container.h"  // from @com_google_absl
 #include "absl/base/call_once.h"  // from @com_google_absl
 #include "absl/base/no_destructor.h"  // from @com_google_absl
 #include "absl/container/flat_hash_map.h"  // from @com_google_absl
@@ -34,6 +35,7 @@ limitations under the License.
 #include "absl/status/status.h"  // from @com_google_absl
 #include "absl/status/statusor.h"  // from @com_google_absl
 #include "absl/strings/str_cat.h"  // from @com_google_absl
+#include "absl/strings/str_format.h"  // from @com_google_absl
 #include "absl/strings/string_view.h"  // from @com_google_absl
 #include "absl/types/span.h"  // from @com_google_absl
 #include "tensor/backends/xnnpack/arithmetic.h"
@@ -428,6 +430,11 @@ absl::StatusOr<uint32_t> XnnpackBuildContext::DefineValue(
     KeepAlive(info.buffer);
   }
 
+  if (absl::c_any_of(info.shape, [](int d){return d < 0;})) {
+    return absl::InvalidArgumentError(absl::StrFormat(
+             "%s: tensor has negative dimension; dynamic shapes are not "
+             "supported in the XNNPACK backend", info.name));
+  }
   std::vector<size_t> dims(info.shape.begin(), info.shape.end());
   const void* data_ptr = value.data.data();
 
