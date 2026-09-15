06703bff2c6b11021f56ebc8ace5b04d8028c25e
Quentin Khan <qkhan@google.com>
2026-09-15 14:28:28 -0700

    Get the start/end of turn prompt from the tokenizer.
    We were using the wrong identifiers that were copied from Gemma 3.
    
    LiteRT-PiperOrigin-RevId: 982043067

--- tensor/examples/gemma4/xnnpack_main.cc
+++ tensor/examples/gemma4/xnnpack_main.cc
@@ -722,10 +722,13 @@ absl::Status Run(const std::string& weights_path,
   const Config config = Config::From(model_variant);
 
   std::string prompt = raw_prompt;
-  if (model_variant == ModelVariant::kE4B &&
-      !absl::StrContains(raw_prompt, "<start_of_turn>")) {
-    prompt = absl::StrCat("<start_of_turn>user\n", raw_prompt,
-                          "<end_of_turn>\n<start_of_turn>model\n");
+  if (const std::string start_of_turn =
+          tokenizer.DecodeToken(kStartOfTurnToken);
+      model_variant == ModelVariant::kE4B &&
+      !absl::StrContains(raw_prompt, start_of_turn)) {
+    prompt = absl::StrCat(start_of_turn, "user\n", raw_prompt,
+                          tokenizer.DecodeToken(kEndOfTurnToken), "\n",
+                          start_of_turn, "model\n");
   }
 
   ABSL_LOG(INFO) << "Using Gemma4 " << AbslUnparseFlag(model_variant)
@@ -761,6 +764,7 @@ absl::Status Run(const std::string& weights_path,
   TRACE_EVENT_BEGIN(kTensorApiCategory, "TokenizerEncode");
   std::vector<int32_t> input_tokens =
       tokenizer.Encode(prompt, /*add_bos=*/true);
+
   TRACE_EVENT_END(kTensorApiCategory);
   int seq_len = static_cast<int>(input_tokens.size());
 
