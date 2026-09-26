909b5fb11bcea26cc5d8caeaf261791fe9f3b5ad
Terry Heo <terryheo@google.com>
2026-09-26 12:21:53 -0700

    Build TFLite kernel tests without TensorFlow in CMake
    The CMake kernel tests were the last user of the TensorFlow checkout. They
    now build from LiteRT sources only.
    
    - Took the schema and converter sources from tflite/converter and generated
      the mutable schema header in the build directory.
    - Dropped the unused compiler/mlir/tools/optimize/quantization_utils.cc and
      the tsl logging sources.
    - Replaced the tsl logging include in kernels/test_util with absl/log.
    - Added TFLITE_WITHOUT_MODEL_DUMP to compile out test model dumping, which
      needs tools/serialization, and set it in the CMake build.
    - Fixed stale sources and link deps (op_version, op_signature, absl
      check/log/random).
    - Removed the TensorFlow FetchContent block and TENSORFLOW_SOURCE_DIR.
    
    PiperOrigin-RevId: 988908594

--- tflite/kernels/test_util.h
+++ tflite/kernels/test_util.h
@@ -42,6 +42,8 @@ limitations under the License.
 #include "absl/algorithm/container.h"
 #include "absl/log/absl_check.h"
 #include "absl/log/absl_log.h"
+#include "absl/log/check.h"
+#include "absl/log/log.h"
 #include "absl/types/span.h"
 #include "Eigen/Core"  // from @eigen_archive
 #include "flatbuffers/buffer.h"  // from @flatbuffers
@@ -63,7 +65,6 @@ limitations under the License.
 #include "tflite/types/fp16.h"
 #include "tflite/types/half.h"
 #include "tflite/util.h"
-#include "tsl/platform/logging.h"
 
 namespace tflite {
 
