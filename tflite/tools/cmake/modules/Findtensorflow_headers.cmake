7994e0a0b514cd7ea1564e182a7b997dc8af9229
Terry Heo <terryheo@google.com>
2026-09-25 19:56:32 -0700

    Build benchmark_model without protobuf in TFLite CMake
    The CMake benchmark_model build fetched the TensorFlow repository to compile
    tf.Example and TFLite profiling protos. Compile the proto-based outputs out
    instead, so benchmark_model builds without TensorFlow or protobuf.
    
    - Added the TFLITE_WITHOUT_PROTO define to benchmark_tflite_model.cc and
      profile_summary_formatter.{h,cc}, guarding the proto-based outputs
      (--output_proto_filepath, --result_file_path, --export_model_runtime_info
      and the 'proto' op profiling output mode).
    - Set TFLITE_WITHOUT_PROTO for benchmark_model and label_image in CMake and
      dropped their proto link dependencies.
    - Removed the CMake files for the benchmark result and profiling protos.
    - Downloaded stats_calculator and its forwarding headers in
      Findtensorflow_headers.cmake instead of taking them from the TensorFlow
      checkout.
    - Validated the field count of each Findtensorflow_headers.cmake entry.
    - Fetched TensorFlow only for the kernel tests.
    - Removed the unused TFLITE_GENERATED_HEADERS_DIR variable.
    
    PiperOrigin-RevId: 988641036

--- tflite/tools/cmake/modules/Findtensorflow_headers.cmake
+++ tflite/tools/cmake/modules/Findtensorflow_headers.cmake
@@ -13,9 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Downloads the few header-only files from the TensorFlow repository that the
-# TFLite runtime includes (xla/tsl/... and third_party/fft2d/...), instead of
-# cloning the whole TensorFlow repository.
+# Downloads the few files from the TensorFlow repository that the TFLite runtime
+# and the benchmark_model tool use (xla/tsl/..., tensorflow/core/util/... and
+# third_party/fft2d/...), instead of cloning the whole TensorFlow repository.
 #
 # Sets TENSORFLOW_HEADERS_INCLUDE_DIR.
 
@@ -36,10 +36,20 @@ set(_tensorflow_headers_files
   "third_party/xla/xla/tsl/framework/convolution/eigen_spatial_convolutions-inl.h|xla/tsl/framework/convolution/eigen_spatial_convolutions-inl.h|c0bec189723d52b4495dec62e197e2a4ffe7725bc313676984384e8b8fbb7a13"
   "third_party/xla/xla/tsl/lib/random/philox_random.h|xla/tsl/lib/random/philox_random.h|7a7659f95c59419373261af305736311bfd839b281259703b23f4df9b391a43e"
   "third_party/xla/xla/tsl/lib/random/random_distributions_utils.h|xla/tsl/lib/random/random_distributions_utils.h|1d65158a878510a1ec5835c5f26962b0ebe45bd56ecaec7e38314a8ac5e7a517"
+  # Used by the benchmark_model tool.
+  "tensorflow/core/util/stat_summarizer_options.h|tensorflow/core/util/stat_summarizer_options.h|e91f94b2fbb7e3a8d8b99664a1ff2443088ca1433b4372b1fcd2477b78492b3b"
+  "tensorflow/core/util/stats_calculator.h|tensorflow/core/util/stats_calculator.h|8d80ca00444162a55a288b3b7369f314e5cf65a3258387aef4af1df39db08e0a"
+  "third_party/xla/xla/tsl/util/stat_summarizer_options.h|xla/tsl/util/stat_summarizer_options.h|9f7d7cc5de38ae6e97a8982dc386a012ed70144c768ef2525fd25054de41c2b2"
+  "third_party/xla/xla/tsl/util/stats_calculator.cc|xla/tsl/util/stats_calculator.cc|704c6d22240a521a7cbee591cd932121f0f6dcbaf179037758a7f3bd4d851e82"
+  "third_party/xla/xla/tsl/util/stats_calculator.h|xla/tsl/util/stats_calculator.h|f5628ba1fbf39e7c4d2b280ff78c8daba40d9d462a97f8aa2abc0c5371b9f241"
 )
 
 foreach(_entry IN LISTS _tensorflow_headers_files)
   string(REPLACE "|" ";" _entry "${_entry}")
+  list(LENGTH _entry _entry_len)
+  if(NOT _entry_len EQUAL 3)
+    message(FATAL_ERROR "Malformed entry in _tensorflow_headers_files: ${_entry}")
+  endif()
   list(GET _entry 0 _src)
   list(GET _entry 1 _path)
   list(GET _entry 2 _sha256)
